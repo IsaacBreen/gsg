7abec0b4b62ff49d5c1713cec86b6fdb0a5a1eed
IsaacBreen <mail@isaacbreen.com>
2024-06-28 17:38:30 +0800

    very very nice

--- jun0/src/lib.rs
+++ jun0/src/lib.rs
@@ -105,16 +105,14 @@ impl FromIterator<u8> for u8set {
     }
 }
 
-// Trait for the parser state
 pub trait ParserState: Clone {
     fn new() -> Self;
-    fn parse(&mut self, read_char: &impl ReadChar);
-    fn valid_next_chars(&self) -> u8set;
+    fn parse<F: Readu8>(&mut self, read_u8: &F);
+    fn valid_next_u8set(&self) -> u8set;
     fn is_valid(&self) -> bool {
-        !self.valid_next_chars().is_empty()
+        !self.valid_next_u8set().is_empty()
     }
 }
 
-// Trait for reading characters
-pub trait ReadChar: Fn(usize) -> Option<char> {}
-impl<F: Fn(usize) -> Option<char>> ReadChar for F {}
+pub trait Readu8: Fn(usize) -> Option<u8> {}
+impl<F: Fn(usize) -> Option<u8>> Readu8 for F {}
