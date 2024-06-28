ff9f8d5fc237f785a7cdc8acb2dc01b2cad9be53
IsaacBreen <mail@isaacbreen.com>
2024-06-28 22:40:06 +0800

    noice

--- jun0/src/combinators/enum.rs
+++ jun0/src/combinators/enum.rs
@@ -17,13 +17,6 @@ macro_rules! visit {
     };
 }
 
-impl<S1: ParserState, S2: ParserState> ParserState for EnumCombinator<S1, S2> {
-    fn new(position: usize) -> Self { EnumCombinator::S1(S1::new(position)) }
-    fn parse<F: Readu8>(&mut self, reader: &F) { visit!(self, state => state.parse(reader)) }
-    fn valid_next_u8set(&self) -> u8set { visit!(self, state => state.valid_next_u8set()) }
-    fn position(&self) -> usize { visit!(self, state => state.position()) }
-}
-
 impl<S1: ParserState, S2: ParserState> EnumCombinator<S1, S2> {
     pub fn init_next(&mut self, position: usize) -> bool {
         let new_state = match self {
@@ -33,4 +26,11 @@ impl<S1: ParserState, S2: ParserState> EnumCombinator<S1, S2> {
         *self = EnumCombinator::S2(new_state);
         true
     }
+}
+
+impl<S1: ParserState, S2: ParserState> ParserState for EnumCombinator<S1, S2> {
+    fn new(position: usize) -> Self { EnumCombinator::S1(S1::new(position)) }
+    fn parse<F: Readu8>(&mut self, reader: &F) { visit!(self, state => state.parse(reader)) }
+    fn valid_next_u8set(&self) -> u8set { visit!(self, state => state.valid_next_u8set()) }
+    fn position(&self) -> usize { visit!(self, state => state.position()) }
 }
