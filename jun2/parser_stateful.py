79eea5f398a2b2177fb7c3f8749fec1f0285c37d
IsaacBreen <mail@isaacbreen.com>
2024-07-01 15:26:16 +0800

    import gen

--- jun2/parser_stateful.py
+++ jun2/parser_stateful.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Callable, Any, Optional, List
+from typing import Callable, Any, Optional, List, Generator
 
 import pytest
 
