5926f4918328d9ac3c5f4875219d2b7bb9272b7b
IsaacBreen <mail@isaacbreen.com>
2024-07-02 23:22:04 +0800

    nicenicenice

--- jun1/src/parser.rs
+++ jun1/src/parser.rs
@@ -652,10 +652,10 @@ mod json_parser {
             "GeneratedCSV_mini.json",
             "GeneratedCSV_1.json",
             "GeneratedCSV_2.json",
-            // "GeneratedCSV_10.json",
-            // "GeneratedCSV_20.json",
-            // "GeneratedCSV_100.json",
-            // "GeneratedCSV_200.json",
+            "GeneratedCSV_10.json",
+            "GeneratedCSV_20.json",
+            "GeneratedCSV_100.json",
+            "GeneratedCSV_200.json",
         ];
 
         // Print execution times for each parser
