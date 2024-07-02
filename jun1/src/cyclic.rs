6d9a13a35f24564e601ea28dc5a0676737c19149
IsaacBreen <mail@isaacbreen.com>
2024-07-02 11:32:12 +0800

    f

--- jun1/src/cyclic.rs
+++ jun1/src/cyclic.rs
@@ -1,43 +1,26 @@
-use std::pin::Pin;
-use std::marker::PhantomPinned;
+use std::sync::{Arc, Mutex};
 
-pub struct ForwardRef<T> {
-    inner: Option<Pin<Box<T>>>,
-    _pin: PhantomPinned,
+#[derive(Clone)]
+struct ForwardRef<A> {
+    inner: Option<Arc<Mutex<A>>>,
 }
 
-impl<T> ForwardRef<T> {
-    pub fn new() -> Pin<Box<Self>> {
-        Box::pin(Self {
-            inner: None,
-            _pin: PhantomPinned,
-        })
+impl<A> ForwardRef<A> {
+    fn new() -> Self {
+        Self { inner: None }
     }
 
-    pub fn set(self: Pin<&mut Self>, value: T) {
-        // SAFETY: We're not moving any pinned fields
-        let this = unsafe { self.get_unchecked_mut() };
-        this.inner = Some(Box::pin(value));
+    fn set(&mut self, value: A) {
+        self.inner = Some(Arc::new(Mutex::new(value)));
     }
 
-    pub fn get(self: Pin<&Self>) -> Option<&T> {
-        // SAFETY: We're not moving the pinned value
-        self.inner.as_ref().map(|pinned| pinned.as_ref())
+    fn get(&self) -> Arc<Mutex<A>> {
+        self.inner.as_ref().unwrap().clone()
     }
 }
 
-pub struct Container<T> {
-    a: Pin<Box<ForwardRef<T>>>,
-    _pin: PhantomPinned,
-}
-
-impl<T> Container<T> {
-    pub fn new() -> Pin<Box<Self>> {
-        Box::pin(Self {
-            a: ForwardRef::new(),
-            _pin: PhantomPinned,
-        })
-    }
+struct Container<A> {
+    a: ForwardRef<A>,
 }
 
 #[cfg(test)]
@@ -46,23 +29,8 @@ mod tests {
 
     #[test]
     fn test_forward_ref() {
-        let mut container = Container::new();
-        let forward_ref = container.as_mut().a.as_mut();
-
-        // Now we can set the value without causing infinite size issues
-        forward_ref.set(42);
-
-        assert_eq!(container.a.as_ref().get(), Some(&42));
-    }
-
-    #[test]
-    fn test_cyclic_reference() {
-        let mut container = Container::<Container<()>>::new();
-        let forward_ref = container.as_mut().a.as_mut();
-
-        // Create a cyclic reference
-        forward_ref.set(Container::new());
-
-        assert!(container.a.as_ref().get().is_some());
+        let mut a = ForwardRef::new();
+        let a_final = Container { a: a.clone() };
+        a.set(a_final);
     }
 }
