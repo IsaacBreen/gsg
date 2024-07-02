e9a57111e8d53753930b3bbab79b603cc99f8be0
IsaacBreen <mail@isaacbreen.com>
2024-07-02 11:36:02 +0800

    ffs

--- jun1/src/cyclic.rs
+++ jun1/src/cyclic.rs
@@ -1,26 +1,44 @@
-use std::sync::{Arc, Mutex};
+use std::pin::Pin;
+use std::marker::PhantomPinned;
 
-#[derive(Clone)]
-struct ForwardRef<A> {
-    inner: Option<Arc<Mutex<A>>>,
+struct ForwardRef<T> {
+    inner: Option<Pin<Box<T>>>,
+    _pin: PhantomPinned,
 }
 
-impl<A> ForwardRef<A> {
-    fn new() -> Self {
-        Self { inner: None }
+impl<T> ForwardRef<T> {
+    fn new() -> Pin<Box<Self>> {
+        Box::pin(Self {
+            inner: None,
+            _pin: PhantomPinned,
+        })
     }
 
-    fn set(&mut self, value: A) {
-        self.inner = Some(Arc::new(Mutex::new(value)));
+    fn set(self: Pin<&mut Self>, value: Pin<Box<T>>) {
+        // SAFETY: We're not moving any pinned fields
+        unsafe {
+            self.get_unchecked_mut().inner = Some(value);
+        }
     }
 
-    fn get(&self) -> Arc<Mutex<A>> {
-        self.inner.as_ref().unwrap().clone()
+    fn get(self: Pin<&Self>) -> Option<&T> {
+        // SAFETY: We're not moving the pinned value
+        self.inner.as_ref().map(|pinned| pinned.as_ref())
     }
 }
 
-struct Container<A> {
-    a: ForwardRef<A>,
+struct Container<T> {
+    a: ForwardRef<T>,
+    _pin: PhantomPinned,
+}
+
+impl<T> Container<T> {
+    fn new(a: ForwardRef<T>) -> Pin<Box<Self>> {
+        Box::pin(Self {
+            a,
+            _pin: PhantomPinned,
+        })
+    }
 }
 
 #[cfg(test)]
@@ -28,9 +46,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_forward_ref() {
+    fn test_cyclic_reference() {
         let mut a = ForwardRef::new();
-        let a_final = Container { a: a.clone() };
-        a.set(a_final);
+        let mut container = Container::new(ForwardRef::new());
+
+        // Set the value (still requires careful handling)
+        let pinned_container = container.as_mut();
+        a.as_mut().set(Box::pin(pinned_container));
+
+        // Accessing the value
+        let retrieved_container = a.get();
+        // ...
     }
 }
