f80a271f1b9e225904f594f4bd9b2dcd97d33368
IsaacBreen <mail@isaacbreen.com>
2024-07-02 16:53:53 +0800

    hm

--- jun1/src/parser.rs
+++ jun1/src/parser.rs
@@ -126,9 +126,6 @@ impl Combinator {
             (Combinator::Choice(combinators), CombinatorState::Choice(its)) => {
                 let mut final_result = ParserIterationResult::new(U8Set::none(), false);
                 for (combinator, its) in combinators.iter().zip(its.iter_mut()) {
-                    if its.is_empty() {
-                        continue;
-                    }
                     let result = process(combinator, c, its);
                     final_result |= result;
                 }
