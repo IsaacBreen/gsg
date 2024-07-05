ffda02e2bf917aeafcd730efd1ac43611cc275f4
IsaacBreen <mail@isaacbreen.com>
2024-07-05 15:56:36 +0800

    f

--- jun1/src/combinators/choice.rs
+++ jun1/src/combinators/choice.rs
@@ -3,10 +3,11 @@ use crate::helper_functions::process;
 use crate::parse_iteration_result::{FrameStack, ParserIterationResult};
 use crate::state::CombinatorState;
 use crate::U8Set;
+use std::rc::Rc;
 
 pub struct Choice<C>(pub Vec<C>);
 
-impl<C> Combinator for Choice<C>
+impl<C> Combinator for Choice<Rc<C>>
 where
     C: Combinator<State = Box<dyn CombinatorState>> + 'static,
 {
@@ -17,31 +18,40 @@ where
             its: self
                 .0
                 .iter()
-                .map(|combinator| vec![combinator.initial_state(signal_id, frame_stack.clone())])
+                .map(|combinator| {
+                    vec![combinator.initial_state(signal_id, frame_stack.clone())]
+                })
                 .collect(),
         })
     }
 
     fn next_state(&self, state: &mut Self::State, c: Option<char>, signal_id: &mut usize) -> ParserIterationResult {
-        let mut final_result =
-            ParserIterationResult::new(U8Set::none(), false, FrameStack::default());
+        let mut final_result = ParserIterationResult::new(U8Set::none(), false, FrameStack::default());
         for (combinator, its) in self.0.iter().zip(state.its.iter_mut()) {
-            final_result.merge_assign(process(combinator, c, its, signal_id));
+            let mut tmp = combinator.initial_state(signal_id, FrameStack::default());
+            let res = process(combinator, c, &mut tmp, signal_id);
+            final_result.merge_assign(res);
+            if res.u8set().len() != 0 {
+                its.push(tmp);
+            }
         }
         final_result
     }
 }
 
-pub struct ChoiceState<State> {
-    pub its: Vec<Vec<State>>,
+pub struct ChoiceState<S> {
+    pub its: Vec<Vec<S>>,
 }
 
-impl<State: CombinatorState + 'static> CombinatorState for ChoiceState<State> {
+impl<S> CombinatorState for Box<ChoiceState<S>>
+where
+    S: CombinatorState
+{
     fn as_any(&self) -> &dyn std::any::Any {
-        self
+        self.as_ref() as &dyn std::any::Any
     }
 
     fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
-        self
+        self.as_mut() as &mut dyn std::any::Any
     }
 }
--- jun1/src/state.rs
+++ jun1/src/state.rs
@@ -6,7 +6,10 @@ pub trait CombinatorState {
     fn as_any_mut(&mut self) -> &mut dyn std::any::Any;
 }
 
-impl CombinatorState for Box<dyn CombinatorState> {
+impl<T> CombinatorState for Box<T> 
+where
+    T: CombinatorState + ?Sized
+{
     fn as_any(&self) -> &dyn std::any::Any {
         self.as_ref() as &dyn std::any::Any
     }
