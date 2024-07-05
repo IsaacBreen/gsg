f41df999ae287bcdf6589622333f177ddcd5aef6
IsaacBreen <mail@isaacbreen.com>
2024-07-05 22:59:53 +0800

    frick

--- jun3/src/combinators/forward_ref.rs
+++ jun3/src/combinators/forward_ref.rs
@@ -2,24 +2,26 @@ use std::rc::Rc;
 use crate::{Combinator, ParseData, Parser};
 
 #[derive(Clone)]
-pub struct ForwardRef {
-    a: Option<Rc<dyn Combinator<Parser=Box<dyn Parser>>>>,
+pub struct ForwardRef<P> {
+    a: Option<Rc<dyn Combinator<Parser = P>>>,
 }
 
-impl Combinator for ForwardRef {
+impl<P: Parser> Combinator for ForwardRef<P> {
     type Parser = Box<dyn Parser>;
 
     fn parser(&self, parse_data: ParseData) -> Self::Parser {
-        self.a.as_ref().expect("ForwardRef::parser called before parser").as_ref().parser(parse_data)
+        let x = self.a.as_ref().expect("ForwardRef::parser called before parser").as_ref().parser(parse_data);
+        // Downcast is safe because we know that the parser is a Box<dyn Parser>
+        Box::new(x)
     }
 }
 
-pub fn forward_ref() -> ForwardRef {
+pub fn forward_ref<P>() -> ForwardRef<P> {
     ForwardRef { a: None, }
 }
 
-impl ForwardRef {
-    pub fn set<A: Combinator>(&mut self, a: A) {
+impl<P> ForwardRef<P> {
+    pub fn set<A: Combinator<Parser = P>>(&mut self, a: A) {
         self.a = Some(Rc::new(a));
     }
 }
