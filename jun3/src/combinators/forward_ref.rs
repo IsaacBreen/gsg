e7dc9626c5f58d582826994ba4fb98c6e9c60762
IsaacBreen <mail@isaacbreen.com>
2024-07-06 10:51:14 +0800

    hm

--- jun3/src/combinators/forward_ref.rs
+++ jun3/src/combinators/forward_ref.rs
@@ -6,11 +6,12 @@ pub struct ForwardRef {
     a: Option<Rc<dyn Combinator<Parser = dyn Parser>>>,
 }
 
-impl<P: Parser> Combinator for ForwardRef {
+impl Combinator for ForwardRef {
     type Parser = Box<dyn Parser>;
 
     fn parser(&self, parse_data: ParseData) -> Self::Parser {
-        let x = self.a.as_ref().expect("ForwardRef::parser called before parser").as_ref().parser(parse_data);
+        let x = self.a.as_ref().expect("ForwardRef::parser called before parser");
+        let p = x.as_ref().parser(parse_data);
         // Downcast is safe because we know that the parser is a Box<dyn Parser>
         Box::new(x)
     }
