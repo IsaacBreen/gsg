6e21d2733ba8b701315f0e7292489a1a505e9b3c
IsaacBreen <mail@isaacbreen.com>
2024-07-06 11:24:09 +0800

    hm

--- jun3/src/tests/make_dyn_work.rs
+++ jun3/src/tests/make_dyn_work.rs
@@ -2,10 +2,29 @@ use std::cell::RefCell;
 use std::rc::Rc;
 
 // Define traits
-trait AssocTrait {}
+trait AssocTrait {
+    fn foo(&self, n: usize) -> usize;
+}
 
 trait Trait {
     type Assoc: AssocTrait;
+    fn get_assoc(&self) -> &Self::Assoc;
+}
+
+// Define some dummy types
+struct FooAssoc;
+struct Foo;
+
+impl AssocTrait for FooAssoc {
+    fn foo(&self, n: usize) -> usize {
+        n
+    }
+}
+impl Trait for Foo {
+    type Assoc = FooAssoc;
+    fn get_assoc(&self) -> &Self::Assoc {
+        &FooAssoc
+    }
 }
 
 // Forward references are useful for recursive definitions.
@@ -25,13 +44,34 @@ impl ForwardRef {
     }
 }
 
-// Container could be anything, including a user-defined type. We cannot hardcode it.
-struct Container<T> {
-    inner: T,
+impl Trait for ForwardRef {
+    type Assoc = dyn AssocTrait;
+    fn get_assoc(&self) -> &Self::Assoc {
+        self.inner.borrow().as_ref().unwrap().get_assoc()
+    }
+}
+
+// Pair could contain anything, including user-defined types. We cannot hardcode it.
+struct Pair<A, B> {
+    a: A,
+    b: B,
+}
+struct PairAssoc<AAssoc, BAssoc> {
+    a: AAssoc,
+    b: BAssoc,
+}
+
+impl<A: Trait<Assoc = AAssoc>, B: Trait<Assoc = BAssoc>, AAssoc: AssocTrait, BAssoc: AssocTrait> Pair<A, B> {
+    fn get_assoc(&self) -> PairAssoc<&AAssoc, &BAssoc> {
+        PairAssoc { a: self.a.get_assoc(), b: self.b.get_assoc() }
+    }
 }
 
 fn main() {
     let mut forward_ref = ForwardRef::new();
-    let inner = Box::new(Container { inner: forward_ref.clone() });
-    forward_ref.set(inner);
+    let a = Foo;
+    let b = forward_ref.clone();
+    let pair = Pair { a, b };
+    let assoc = pair.get_assoc();
+    assoc.foo();
 }
