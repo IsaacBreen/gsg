ecc3093540bb792ff51c95e67f6a8f56cff09e1a
IsaacBreen <mail@isaacbreen.com>
2024-07-06 12:30:09 +0800

    wow. works.

--- jun3/src/combinators/forward_ref.rs
+++ jun3/src/combinators/forward_ref.rs
@@ -1,25 +1,41 @@
 use std::rc::Rc;
+use std::cell::RefCell;
 use crate::{Combinator, ParseData, Parser};
 
 #[derive(Clone)]
 pub struct ForwardRef {
-    a: Option<Rc<dyn Combinator<Parser = Box<dyn Parser>>>>,
+    a: Rc<RefCell<Option<Rc<dyn Combinator<Parser = Box<dyn Parser>>>>>>,
 }
 
 impl Combinator for ForwardRef {
     type Parser = Box<dyn Parser>;
 
     fn parser(&self, parse_data: ParseData) -> Self::Parser {
-        self.a.as_ref().expect("ForwardRef::parser called before parser").parser(parse_data)
+        self.a.borrow().as_ref().expect("ForwardRef::parser called before parser").parser(parse_data)
     }
 }
 
 pub fn forward_ref() -> ForwardRef {
-    ForwardRef { a: None, }
+    ForwardRef { a: Rc::new(RefCell::new(None)) }
 }
 
 impl ForwardRef {
     pub fn set<A: Combinator<Parser = P> + 'static, P: Parser + 'static>(&mut self, a: A) {
-        self.a = Some(Rc::new(a));
+        let boxed: Rc<dyn Combinator<Parser=Box<dyn Parser>>> = Rc::new(Wrapper(a));
+        *self.a.borrow_mut() = Some(boxed);
+    }
+}
+
+struct Wrapper<T>(T);
+
+impl<T, P> Combinator for Wrapper<T>
+where
+    T: Combinator<Parser = P>,
+    P: Parser + 'static,
+{
+    type Parser = Box<dyn Parser>;
+
+    fn parser(&self, parse_data: ParseData) -> Self::Parser {
+        Box::new(self.0.parser(parse_data))
     }
 }
