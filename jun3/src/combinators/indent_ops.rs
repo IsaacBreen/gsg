f215cfa3c9116fd2a38c623ac117927b3f145b8d
IsaacBreen <mail@isaacbreen.com>
2024-07-08 19:06:42 +0800

    hmmmmmmm

--- jun3/src/combinators/indent_ops.rs
+++ jun3/src/combinators/indent_ops.rs
@@ -1,4 +1,4 @@
-use crate::Seq2;
+use crate::{Seq2, U8Set};
 use crate::{Combinator, ParseData, ParseDataExt, Parser, ParseResult, seq};
 
 #[derive(Clone, PartialEq, Debug, Default)]
@@ -9,16 +9,19 @@ pub struct IndentTrackers {
 #[derive(Clone, PartialEq, Debug)]
 pub struct IndentTracker {
     i: usize,
-    indents: Vec<Vec<u8>>
+    indents: Vec<Vec<u8>>,
+    dedent_counter: usize,
 }
 
 impl ParseDataExt for IndentTrackers {
     fn merge(self, other: Self) -> Self {
-        todo!()
+        let mut merged = self.indent_trackers;
+        merged.extend(other.indent_trackers);
+        IndentTrackers { indent_trackers: merged }
     }
 
     fn forward(self, other: Self) -> Self {
-        todo!()
+        other
     }
 }
 
@@ -39,7 +42,8 @@ impl Default for IndentTracker {
     fn default() -> Self {
         IndentTracker {
             i: 0,
-            indents: vec![vec![]]
+            indents: vec![vec![]],
+            dedent_counter: 0,
         }
     }
 }
@@ -52,28 +56,79 @@ impl IndentTracker {
     pub fn pop(&mut self) {
         self.indents.pop();
     }
+
+    pub fn track(&mut self, c: u8) -> bool {
+        if self.i < self.indents.len() {
+            let indent_level = &self.indents[self.i];
+            if self.i == indent_level.len() {
+                self.i += 1;
+                true
+            } else if indent_level[self.i] == c {
+                self.i += 1;
+                true
+            } else {
+                self.dedent_counter += self.indents.len() - self.i;
+                self.i = self.indents.len();
+                false
+            }
+        } else {
+            false
+        }
+    }
 }
 
 pub struct Indent {}
-pub struct IndentParser {}
+pub struct IndentParser {
+    level: Vec<u8>,
+    pos: usize,
+    parse_data: Option<ParseData>,
+}
 pub struct Dedent {}
-pub struct DedentParser {}
+pub struct DedentParser {
+    parse_data: Option<ParseData>,
+}
 pub struct Newline {}
-pub struct NewlineParser {}
+pub struct NewlineParser {
+    parse_data: Option<ParseData>,
+}
 pub struct ValidateIndent {}
-pub struct ValidateIndentParser {}
+pub struct ValidateIndentParser {
+    parse_data: Option<ParseData>,
+}
 
 impl Combinator for Indent {
     type Parser = IndentParser;
 
     fn parser(&self, parse_data: ParseData) -> (Self::Parser, ParseResult) {
-        todo!();
+        let indent_tracker = parse_data.indent_tracker.clone().unwrap();
+        let mut new_tracker = indent_tracker.clone();
+        new_tracker.push();
+        let mut new_parse_data = parse_data.clone();
+        new_parse_data.indent_tracker = Some(new_tracker);
+        (
+            IndentParser {
+                level: vec![],
+                pos: 0,
+                parse_data: Some(new_parse_data),
+            },
+            ParseResult::new(U8Set::none(), Some(parse_data)),
+        )
     }
 }
 
 impl Parser for IndentParser {
     fn step(&mut self, c: u8) -> ParseResult {
-        todo!();
+        if self.pos < self.level.len() {
+            if self.level[self.pos] == c {
+                self.pos += 1;
+                ParseResult::new(U8Set::none(), self.parse_data.clone())
+            } else {
+                ParseResult::default()
+            }
+        } else {
+            self.level.push(c);
+            ParseResult::new(U8Set::none(), self.parse_data.clone())
+        }
     }
 }
 
@@ -81,14 +136,35 @@ impl Combinator for Dedent {
     type Parser = DedentParser;
 
     fn parser(&self, parse_data: ParseData) -> (Self::Parser, ParseResult) {
-        todo!();
+        let indent_tracker = parse_data.indent_tracker.clone().unwrap();
+        let mut new_tracker = indent_tracker.clone();
+        new_tracker.pop();
+        let mut new_parse_data = parse_data.clone();
+        new_parse_data.indent_tracker = Some(new_tracker);
+        (
+            DedentParser {
+                parse_data: Some(new_parse_data),
+            },
+            ParseResult::new(U8Set::none(), Some(parse_data)),
+        )
     }
-
 }
 
 impl Parser for DedentParser {
-    fn step(&mut self, c: u8) -> ParseResult {
-        todo!();
+    fn step(&mut self, _c: u8) -> ParseResult {
+        if let Some(parse_data) = &mut self.parse_data {
+            if let Some(indent_tracker) = &mut parse_data.indent_tracker {
+                for tracker in &mut indent_tracker.indent_trackers {
+                    if tracker.dedent_counter > 0 {
+                        tracker.dedent_counter -= 1;
+                        return ParseResult::new(U8Set::none(), self.parse_data.clone());
+                    } else {
+                        return ParseResult::default();
+                    }
+                }
+            }
+        }
+        ParseResult::default()
     }
 }
 
@@ -96,13 +172,42 @@ impl Combinator for Newline {
     type Parser = NewlineParser;
 
     fn parser(&self, parse_data: ParseData) -> (Self::Parser, ParseResult) {
-        todo!()
+        (
+            NewlineParser {
+                parse_data: Some(parse_data.clone()),
+            },
+            ParseResult::new(U8Set::from_u8(b'\n'), Some(parse_data)),
+        )
     }
 }
 
 impl Parser for NewlineParser {
     fn step(&mut self, c: u8) -> ParseResult {
-        todo!()
+        if c == b'\n' {
+            if let Some(parse_data) = &mut self.parse_data {
+                if let Some(indent_tracker) = &mut parse_data.indent_tracker {
+                    for tracker in &mut indent_tracker.indent_trackers {
+                        if tracker.dedent_counter == 0 {
+                            tracker.i = 0;
+                        } else {
+                            return ParseResult::default();
+                        }
+                    }
+                }
+            }
+            ParseResult::new(U8Set::from_chars(" \t"), self.parse_data.clone())
+        } else {
+            if let Some(parse_data) = &mut self.parse_data {
+                if let Some(indent_tracker) = &mut parse_data.indent_tracker {
+                    for tracker in &mut indent_tracker.indent_trackers {
+                        if !tracker.track(c) {
+                            return ParseResult::default();
+                        }
+                    }
+                }
+            }
+            ParseResult::new(U8Set::none(), self.parse_data.clone())
+        }
     }
 }
 
@@ -110,13 +215,27 @@ impl Combinator for ValidateIndent {
     type Parser = ValidateIndentParser;
 
     fn parser(&self, parse_data: ParseData) -> (Self::Parser, ParseResult) {
-        todo!()
+        (
+            ValidateIndentParser {
+                parse_data: Some(parse_data.clone()),
+            },
+            ParseResult::new(U8Set::none(), Some(parse_data)),
+        )
     }
 }
 
 impl Parser for ValidateIndentParser {
-    fn step(&mut self, c: u8) -> ParseResult {
-        todo!()
+    fn step(&mut self, _c: u8) -> ParseResult {
+        if let Some(parse_data) = &self.parse_data {
+            if let Some(indent_tracker) = &parse_data.indent_tracker {
+                for tracker in &indent_tracker.indent_trackers {
+                    if tracker.dedent_counter != 0 {
+                        return ParseResult::default();
+                    }
+                }
+            }
+        }
+        ParseResult::new(U8Set::none(), self.parse_data.clone())
     }
 }
 
