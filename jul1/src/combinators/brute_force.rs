a0f03ba54bd9d05b66e377cd3c1617f8bd231e95
IsaacBreen <mail@isaacbreen.com>
2024-07-09 23:12:13 +0800

    nice

--- jul1/src/combinators/brute_force.rs
+++ jul1/src/combinators/brute_force.rs
@@ -1,33 +1,62 @@
 use std::rc::Rc;
 use crate::{ParserTrait, CombinatorTrait, VerticalData, HorizontalData};
 
-pub struct BruteForce<F: Fn(&Vec<u8>, &HorizontalData) -> (Vec<HorizontalData>, Vec<VerticalData>)> {
-    pub f: Rc<F>,
+pub enum DataEnum {
+    Horizontal(HorizontalData),
+    Vertical(VerticalData),
+    None,
 }
 
-pub struct BruteForceParser<F: Fn(&Vec<u8>, &HorizontalData) -> (Vec<HorizontalData>, Vec<VerticalData>)> {
-    pub f: Rc<F>,
+pub type BruteForceFn = fn(&Vec<u8>, &HorizontalData) -> DataEnum;
+
+pub struct BruteForce {
+    pub f: Rc<BruteForceFn>,
+}
+
+pub struct BruteForceParser {
+    pub f: Rc<BruteForceFn>,
     pub values: Vec<u8>,
     pub horizontal_data: HorizontalData,
 }
 
-impl<F: Fn(&Vec<u8>, &HorizontalData) -> (Vec<HorizontalData>, Vec<VerticalData>) + 'static> CombinatorTrait for BruteForce<F> {
-    type Parser = BruteForceParser<F>;
+impl CombinatorTrait for BruteForce {
+    type Parser = BruteForceParser;
 
     fn parser(&self, horizontal_data: HorizontalData) -> (Self::Parser, Vec<HorizontalData>, Vec<VerticalData>) {
-        let (horizontal_data2, vertical_data) = (self.f)(&Vec::new(), &horizontal_data);
-        (BruteForceParser { f: self.f.clone(), values: Vec::new(), horizontal_data }, horizontal_data2, vertical_data)
+        // let (horizontal_data2, vertical_data) = (self.f)(&Vec::new(), &horizontal_data);
+        let data_enum = (self.f)(&Vec::new(), &horizontal_data);
+        let (mut horizontal_data_vec, mut vertical_data_vec) = (vec![], vec![]);
+        match data_enum {
+            DataEnum::Horizontal(horizontal_data) => {
+                horizontal_data_vec.push(horizontal_data);
+            },
+            DataEnum::Vertical(vertical_data) => {
+                vertical_data_vec.push(vertical_data);
+            },
+            DataEnum::None => {},
+        }
+        (BruteForceParser { f: self.f.clone(), values: Vec::new(), horizontal_data }, horizontal_data_vec, vertical_data_vec)
     }
 }
 
-impl<F: Fn(&Vec<u8>, &HorizontalData) -> (Vec<HorizontalData>, Vec<VerticalData>)> ParserTrait for BruteForceParser<F> {
+impl ParserTrait for BruteForceParser {
     fn step(&mut self, c: u8) -> (Vec<HorizontalData>, Vec<VerticalData>) {
         self.values.push(c);
-        let (horizontal_data2, vertical_data) = (self.f)(&self.values, &self.horizontal_data);
-        (horizontal_data2, vertical_data)
+        let data_enum = (self.f)(&self.values, &self.horizontal_data);
+        let (mut horizontal_data_vec, mut vertical_data_vec) = (vec![], vec![]);
+        match data_enum {
+            DataEnum::Horizontal(horizontal_data) => {
+                horizontal_data_vec.push(horizontal_data);
+            },
+            DataEnum::Vertical(vertical_data) => {
+                vertical_data_vec.push(vertical_data);
+            },
+            DataEnum::None => {},
+        }
+        (horizontal_data_vec, vertical_data_vec)
     }
 }
 
-pub fn brute_force<F: Fn(&Vec<u8>, &HorizontalData) -> (Vec<HorizontalData>, Vec<VerticalData>)>(f: F) -> BruteForce<F> {
+pub fn brute_force(f: BruteForceFn) -> BruteForce {
     BruteForce { f: Rc::new(f) }
 }
--- jul1/src/combinators/indent.rs
+++ jul1/src/combinators/indent.rs
@@ -1,3 +1,20 @@
-pub fn indent(indent: u8) -> impl Fn(u8) -> u8 {
-    move |n| n + indent
-}
+use crate::{BruteForceFn, DataEnum, HorizontalData, U8Set, VerticalData};
+
+const INDENT_FN: BruteForceFn = |values: &Vec<u8>, horizontal_data: &HorizontalData| {
+    let mut i = 0;
+    for indent_chunk in &horizontal_data.indents {
+        let values_chunk = &values[i..i + indent_chunk.len()];
+        if values_chunk != indent_chunk {
+            if indent_chunk.starts_with(values_chunk) {
+                // This could be a valid indentation, but we need more
+                let next_u8 = values_chunk.get(indent_chunk.len()).cloned().unwrap();
+                return DataEnum::Vertical(VerticalData { u8set: U8Set::from_u8(next_u8) });
+            } else {
+                // We have invalid indentation
+                return DataEnum::None;
+            }
+        }
+        i += indent_chunk.len();
+    }
+    DataEnum::Horizontal(horizontal_data.clone())
+};
