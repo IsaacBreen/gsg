1ae94e8ab9ed0bc71bab26cf72bcca248f801d21
IsaacBreen <mail@isaacbreen.com>
2024-07-10 12:28:45 +0800

    hmmmmm

--- jul1/src/combinators/frame_stack_ops.rs
+++ jul1/src/combinators/frame_stack_ops.rs
@@ -1,5 +1,19 @@
 use crate::Eps;
 
+pub enum FrameStackOp {
+    WithNewFrame,
+    PushToFrame,
+    PopFromFrame,
+    FrameStackContains,
+}
+
+// pub enum FrameStackOpParser {
+//     WithNewFrame(Eps),
+//     PushToFrame(Eps),
+//     PopFromFrame(Eps),
+//     FrameStackContains(Eps),
+// }
+
 pub fn with_new_frame<T>(parser: T) -> Eps {
     todo!()
 }
