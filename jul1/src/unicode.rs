c2130fe90437224ce1a3cea986472c447189f231
IsaacBreen <mail@isaacbreen.com>
2024-07-18 15:55:01 +0800

    nice!!

--- jul1/src/unicode.rs
+++ jul1/src/unicode.rs
@@ -1,4 +1,11 @@
-pub fn get_unicode_category(name: &str) -> Vec<Vec<u8>> {
-    // Return a list UTF-8 encoded bytestrings for all characters in the given category.
-    todo!()
+use unicode_general_category::GeneralCategory;
+
+pub fn get_unicode_general_category(general_category: GeneralCategory) -> Vec<Vec<u8>> {
+    let mut result = Vec::new();
+
+    for c in '\u{0}'..='\u{10FFFF}' {
+        result.push(c.to_string().into_bytes());
+    }
+
+    result
 }
