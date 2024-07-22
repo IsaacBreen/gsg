0ed0946f95c063d6a543a87c9792cfa78befb938
IsaacBreen <mail@isaacbreen.com>
2024-07-22 18:06:08 +0800

    remove exception

--- jul1/src/combinators/prevent_consecutive_matches.rs
+++ jul1/src/combinators/prevent_consecutive_matches.rs
@@ -55,26 +55,6 @@ impl CombinatorTrait for PreventConsecutiveMatchesSet {
     }
 }
 
-pub struct PreventConsecutiveMatchesException {
-    match_id: String,
-}
-
-impl CombinatorTrait for PreventConsecutiveMatchesException {
-    type Parser = FailParser;
-    fn parser(&self, mut right_data: RightData) -> (Self::Parser, ParseResults) {
-        if right_data.prevent_consecutive_matches.prev_match_ids.contains(&self.match_id) {
-            (FailParser, ParseResults {
-                right_data_vec: vec![right_data],
-                up_data_vec: vec![],
-                cut: false,
-            })
-        } else {
-            right_data.prevent_consecutive_matches.prev_match_ids.push(self.match_id.clone());
-            (FailParser, ParseResults::no_match())
-        }
-    }
-}
-
 pub struct PreventConsecutiveMatchesAdd {
     match_id: String,
 }
@@ -101,10 +81,6 @@ pub fn prevent_consecutive_matches_clear() -> PreventConsecutiveMatchesClear {
 
 pub fn prevent_consecutive_matches_set(match_id: &str) -> PreventConsecutiveMatchesSet {
     PreventConsecutiveMatchesSet { match_id: match_id.to_string() }
-}
-
-pub fn prevent_consecutive_matches_exception(match_id: &str) -> PreventConsecutiveMatchesException {
-    PreventConsecutiveMatchesException { match_id: match_id.to_string() }
 }
 
 pub fn prevent_consecutive_matches_add(match_id: &str) -> PreventConsecutiveMatchesAdd {
