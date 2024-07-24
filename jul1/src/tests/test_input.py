3911a6ebc6513a1e8cdfea640bc8f2057c427255
IsaacBreen <mail@isaacbreen.com>
2024-07-24 17:14:39 +0800

    replace test file

--- jul1/src/tests/test_input.py
+++ jul1/src/tests/test_input.py
@@ -1,1 +1,8 @@
-[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[x]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+class Node(abc.ABC):
+    @abc.abstractmethod
+    def decompose_on_left_recursion(self, ref: Ref) -> tuple[Node, Node]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def replace_left_refs(self, replacements: dict[Ref, Node]) -> Node:
+        raise NotImplementedError
