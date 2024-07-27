471360300620d8b2967595d465d407d1c62b80ac
IsaacBreen <mail@isaacbreen.com>
2024-07-27 17:56:56 +0800

    Revert "nice?!"
    This reverts commit fef283ccb4393c55783fc3146ec7929112e6a129.

---
+++ jul1/src/combinator.rs
@@ -0,0 +1,234 @@
+use std::collections::BTreeMap;
+use std::fmt::{Display, Formatter};
+use std::rc::Rc;
+use std::any::Any;
+use std::hash::Hasher;
+use crate::parse_state::{RightData, UpData};
+use crate::{ParseResults, U8Set};
+
+#[derive(Default, Debug, PartialEq, Eq)]
+pub struct Stats
+{
+    pub active_parser_type_counts: BTreeMap<String, usize>,
+    pub active_symbols: BTreeMap<String, usize>,
+    pub active_tags: BTreeMap<String, usize>,
+    pub active_string_matchers: BTreeMap<String, usize>,
+    pub active_u8_matchers: BTreeMap<U8Set, usize>,
+}
+
+impl Display for Stats {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        fn write_sorted<S: Clone + Display>(f: &mut Formatter, title: &str, items: &[(S, usize)]) -> std::fmt::Result {
+            writeln!(f, "{}", title)?;
+            let mut sorted_items = items.to_vec();
+            sorted_items.sort_by(|a, b| a.1.cmp(&b.1));
+            for (name, count) in sorted_items {
+                let mut name = name.to_string();
+                // Trim it if it's very long
+                if name.len() > 80 {
+                    name.truncate(80);
+                    name.push_str("...");
+                }
+                writeln!(f, "    {}: {}", name, count)?;
+            }
+            writeln!(f, "")
+        }
+
+        write_sorted(f, "Active Parser Types:", self.active_parser_type_counts.clone().into_iter().collect::<Vec<_>>().as_slice())?;
+        // write_sorted(f, "Active Symbols:", self.active_symbols.clone().into_iter().collect::<Vec<_>>().as_slice())?;
+        write_sorted(f, "Active Tags:", self.active_tags.clone().into_iter().collect::<Vec<_>>().as_slice())?;
+        // write_sorted(f, "Active String Matchers:", self.active_string_matchers.clone().into_iter().collect::<Vec<_>>().as_slice())?;
+        // write_sorted(f, "Active U8 Matchers:", self.active_u8_matchers.clone().into_iter().collect::<Vec<_>>().as_slice())?;
+
+        Ok(())
+    }
+}
+
+impl Stats {
+    pub fn total_active_parsers(&self) -> usize {
+        self.active_parser_type_counts.values().sum()
+    }
+
+    pub fn total_active_symbols(&self) -> usize {
+        self.active_symbols.values().sum()
+    }
+
+    pub fn total_active_tags(&self) -> usize {
+        self.active_tags.values().sum()
+    }
+
+    pub fn total_active_string_matchers(&self) -> usize {
+        self.active_string_matchers.values().sum()
+    }
+
+    pub fn total_active_u8_matchers(&self) -> usize {
+        self.active_u8_matchers.values().sum()
+    }
+}
+
+pub trait CombinatorTrait
+where
+    Self: 'static,
+{
+    type Parser: ParserTrait;
+    fn parser(&self, right_data: RightData) -> (Self::Parser, ParseResults);
+    fn into_box_dyn(self) -> Box<DynCombinator>
+    where
+        Self: Sized,
+    {
+        Box::new(DynWrapper(self))
+    }
+    fn into_rc_dyn(self) -> Rc<DynCombinator>
+    where
+        Self: Sized,
+    {
+        Rc::new(DynWrapper(self))
+    }
+    fn dyn_eq(&self, other: &DynCombinator) -> bool { std::ptr::eq(self.as_any(), other.as_any()) }
+    fn dyn_hash(&self, state: &mut dyn Hasher) {}
+    fn as_any(&self) -> &dyn Any;
+}
+
+pub trait ParserTrait {
+    fn step(&mut self, c: u8) -> ParseResults;
+    fn stats(&self) -> Stats {
+        let mut stats = Stats::default();
+        self.collect_stats(&mut stats);
+        stats
+    }
+    fn collect_stats(&self, stats: &mut Stats) {
+        stats.active_parser_type_counts.entry(std::any::type_name::<Self>().to_string()).and_modify(|c| *c += 1).or_insert(1);
+        for child in self.iter_children() {
+            child.collect_stats(stats);
+        }
+    }
+    fn iter_children<'a>(&'a self) -> Box<dyn Iterator<Item = &'a dyn ParserTrait> + 'a> {
+        Box::new(std::iter::empty())
+    }
+    fn iter_children_mut<'a>(&'a mut self) -> Box<dyn Iterator<Item = &'a mut dyn ParserTrait> + 'a> {
+        Box::new(std::iter::empty())
+    }
+    fn gc(&mut self) {
+        for child in self.iter_children_mut() {
+            child.gc();
+        }
+    }
+    fn dyn_eq(&self, other: &dyn ParserTrait) -> bool;
+    fn dyn_hash(&self, state: &mut dyn Hasher) {}
+    fn as_any(&self) -> &dyn Any;
+}
+
+impl ParserTrait for Box<dyn ParserTrait> {
+    fn step(&mut self, c: u8) -> ParseResults {
+        (**self).step(c)
+    }
+
+    fn stats(&self) -> Stats {
+        (**self).stats()
+    }
+
+    fn collect_stats(&self, stats: &mut Stats) {
+        (**self).collect_stats(stats)
+    }
+
+    fn iter_children<'a>(&'a self) -> Box<dyn Iterator<Item=&'a dyn ParserTrait> + 'a> {
+        (**self).iter_children()
+    }
+
+    fn iter_children_mut<'a>(&'a mut self) -> Box<dyn Iterator<Item=&'a mut dyn ParserTrait> + 'a> {
+        (**self).iter_children_mut()
+    }
+
+    fn gc(&mut self) {
+        (**self).gc()
+    }
+
+    fn dyn_eq(&self, other: &dyn ParserTrait) -> bool {
+        (**self).dyn_eq(other)
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl<C> CombinatorTrait for Rc<C>
+where
+    C: CombinatorTrait + ?Sized,
+{
+    type Parser = C::Parser;
+
+    fn parser(&self, right_data: RightData) -> (Self::Parser, ParseResults) {
+        (**self).parser(right_data)
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+struct DynWrapper<T>(T);
+
+impl<T, P> CombinatorTrait for DynWrapper<T>
+where
+    T: CombinatorTrait<Parser=P>,
+    P: ParserTrait + 'static,
+{
+    type Parser = Box<dyn ParserTrait>;
+
+    fn parser(&self, right_data: RightData) -> (Self::Parser, ParseResults) {
+        let (parser, parse_results) = self.0.parser(right_data);
+        (Box::new(parser), parse_results)
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl CombinatorTrait for Box<DynCombinator> {
+    type Parser = Box<dyn ParserTrait>;
+
+    fn parser(&self, right_data: RightData) -> (Self::Parser, ParseResults) {
+        (**self).parser(right_data)
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+pub type DynCombinator = dyn CombinatorTrait<Parser=Box<dyn ParserTrait>>;
+
+pub trait IntoCombinator {
+    type Output: CombinatorTrait;
+    fn into_combinator(self) -> Self::Output;
+}
+
+impl<T> IntoCombinator for T
+where
+    T: CombinatorTrait,
+{
+    type Output = T;
+    fn into_combinator(self) -> Self::Output {
+        self
+    }
+}
+
+impl<T> IntoCombinator for &Rc<T>
+where
+    T: CombinatorTrait,
+{
+    type Output = Rc<T>;
+    fn into_combinator(self) -> Self::Output {
+        self.clone()
+    }
+}
+
+// TODO: why do we need this? Should be able to delete it, but throws an error (try it!)
+impl IntoCombinator for &Rc<DynCombinator> {
+    type Output = Rc<DynCombinator>;
+    fn into_combinator(self) -> Self::Output {
+        self.clone()
+    }
+}
---
+++ jul1/src/parse_state.rs
@@ -0,0 +1,132 @@
+use std::cell::RefCell;
+use std::collections::{BTreeSet, HashSet};
+use std::hash::{Hash, Hasher};
+use std::rc::Rc;
+use derivative::Derivative;
+use crate::{FrameStack, ForbidFollowsData, U8Set};
+use crate::CacheData;
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ParseResults {
+    pub right_data_vec: Vec<RightData>,
+    pub up_data_vec: Vec<UpData>,
+    pub done: bool,
+}
+
+impl ParseResults {
+    pub fn empty_unfinished() -> Self {
+        ParseResults {
+            right_data_vec: vec![],
+            up_data_vec: vec![],
+            done: false,
+        }
+    }
+    pub fn empty_finished() -> Self {
+        ParseResults {
+            right_data_vec: vec![],
+            up_data_vec: vec![],
+            done: true,
+        }
+    }
+
+    pub(crate) fn combine(&mut self, mut p0: ParseResults) {
+        self.right_data_vec.append(&mut p0.right_data_vec);
+        self.up_data_vec.append(&mut p0.up_data_vec);
+        self.done &= p0.done;
+    }
+
+    pub(crate) fn combine_inplace(mut self, p0: ParseResults) -> Self {
+        self.combine(p0);
+        self
+    }
+}
+
+#[derive(Derivative)]
+#[derivative(Debug, Clone, Hash, PartialOrd, Ord, PartialEq, Eq)]
+pub struct RightData {
+    pub frame_stack: Option<FrameStack>,
+    pub indents: Vec<Vec<u8>>,
+    pub dedents: usize,
+    pub scope_count: usize,
+    #[derivative(PartialEq = "ignore", Hash = "ignore", Debug = "ignore")]
+    pub forbidden_consecutive_matches: ForbidFollowsData,
+    pub cache_data: CacheData,
+    #[derivative(PartialEq = "ignore", Hash = "ignore")]
+    pub position: usize,
+}
+
+#[derive(Debug, Default, Clone, PartialEq)]
+pub struct UpData {
+    pub u8set: U8Set,
+}
+
+impl Default for RightData {
+    fn default() -> Self {
+        Self {
+            frame_stack: Some(FrameStack::default()),
+            indents: vec![],
+            dedents: 0,
+            scope_count: 0,
+            forbidden_consecutive_matches: ForbidFollowsData::default(),
+            cache_data: CacheData::default(),
+            position: 0,
+        }
+    }
+}
+
+impl RightData {
+}
+
+pub trait Squash {
+    type Output;
+    fn squashed(self) -> Self::Output;
+    fn squash(&mut self);
+}
+
+impl Squash for Vec<RightData> {
+    type Output = Vec<RightData>;
+    fn squashed(self) -> Self::Output {
+        if self.len() > 1 {
+            self.into_iter().collect::<BTreeSet<_>>().into_iter().collect()
+        } else {
+            self
+        }
+    }
+    fn squash(&mut self) {
+        if self.len() > 1 {
+            *self = self.drain(..).collect::<Self>().squashed()
+        }
+    }
+}
+
+impl Squash for Vec<UpData> {
+    type Output = Vec<UpData>;
+    fn squashed(self) -> Self::Output {
+        let mut u8set = U8Set::none();
+        for vd in self {
+            u8set = u8set.union(&vd.u8set);
+        }
+        if u8set.is_empty() {
+            vec![]
+        } else {
+            vec![UpData { u8set }]
+        }
+    }
+    fn squash(&mut self) {
+        *self = self.clone().squashed();
+    }
+}
+
+impl Squash for ParseResults {
+    type Output = ParseResults;
+    fn squashed(self) -> Self::Output {
+        ParseResults {
+            right_data_vec: self.right_data_vec.squashed(),
+            up_data_vec: self.up_data_vec.squashed(),
+            done: self.done,
+        }
+    }
+    fn squash(&mut self) {
+        *self = self.clone().squashed();
+    }
+}
