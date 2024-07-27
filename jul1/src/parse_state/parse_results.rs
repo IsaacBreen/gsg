fef283ccb4393c55783fc3146ec7929112e6a129
IsaacBreen <mail@isaacbreen.com>
2024-07-27 17:51:42 +0800

    nice?!

---
+++ jul1/src/parse_state/parse_results.rs
@@ -0,0 +1,35 @@
+#[derive(Debug, Clone, PartialEq)]
+pub struct ParseResults {
+    pub right_data_vec: Vec<RightData>,
+    pub up_data_vec: Vec<UpData>,
+    pub done: bool,
+}
+
+impl ParseResults {
+    pub fn empty_unfinished() -> Self {
+        ParseResults {
+            right_data_vec: vec![],
+            up_data_vec: vec![],
+            done: false,
+        }
+    }
+
+    pub fn empty_finished() -> Self {
+        ParseResults {
+            right_data_vec: vec![],
+            up_data_vec: vec![],
+            done: true,
+        }
+    }
+
+    pub(crate) fn combine(&mut self, mut p0: ParseResults) {
+        self.right_data_vec.append(&mut p0.right_data_vec);
+        self.up_data_vec.append(&mut p0.up_data_vec);
+        self.done &= p0.done;
+    }
+
+    pub(crate) fn combine_inplace(mut self, p0: ParseResults) -> Self {
+        self.combine(p0);
+        self
+    }
+}
---
+++ jul1/src/parse_state/squash_trait.rs
@@ -0,0 +1,57 @@
+use std::collections::BTreeSet;
+use crate::parse_state::{RightData, UpData, ParseResults};
+use crate::U8Set;
+
+pub trait Squash {
+    type Output;
+    fn squashed(self) -> Self::Output;
+    fn squash(&mut self);
+}
+
+impl Squash for Vec<RightData> {
+    type Output = Vec<RightData>;
+    fn squashed(self) -> Self::Output {
+        if self.len() > 1 {
+            self.into_iter().collect::<BTreeSet<_>>().into_iter().collect()
+        } else {
+            self
+        }
+    }
+    fn squash(&mut self) {
+        if self.len() > 1 {
+            *self = self.drain(..).collect::<Self>().squashed()
+        }
+    }
+}
+
+impl Squash for Vec<UpData> {
+    type Output = Vec<UpData>;
+    fn squashed(self) -> Self::Output {
+        let mut u8set = U8Set::none();
+        for vd in self {
+            u8set = u8set.union(&vd.u8set);
+        }
+        if u8set.is_empty() {
+            vec![]
+        } else {
+            vec![UpData { u8set }]
+        }
+    }
+    fn squash(&mut self) {
+        *self = self.clone().squashed();
+    }
+}
+
+impl Squash for ParseResults {
+    type Output = ParseResults;
+    fn squashed(self) -> Self::Output {
+        ParseResults {
+            right_data_vec: self.right_data_vec.squashed(),
+            up_data_vec: self.up_data_vec.squashed(),
+            done: self.done,
+        }
+    }
+    fn squash(&mut self) {
+        *self = self.clone().squashed();
+    }
+}
