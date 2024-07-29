794b50bc7e5a1cf52d4b2de4ecb3490496a103d3
IsaacBreen <mail@isaacbreen.com>
2024-07-29 14:33:10 +0800

    Revert "hmmm"
    This reverts commit ce05e1156745f71c636946aa702df44b6173abf5.

--- jul1/src/combinators/seq.rs
+++ jul1/src/combinators/seq.rs
@@ -1,6 +1,6 @@
 use std::rc::Rc;
 
-use crate::{Combinator, CombinatorTrait, eps, Parser, ParseResults, ParserTrait, RightData, UpData};
+use crate::{Combinator, CombinatorTrait, eps, Parser, ParseResults, ParserTrait, RightData};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Seq {
@@ -14,34 +14,30 @@ pub struct SeqParser {
 
 impl CombinatorTrait for Seq {
     fn parser(&self, right_data: RightData) -> (Parser, ParseResults) {
-        // let mut children = Vec::new();
+        let mut children = Vec::new();
         let mut current_right_data = vec![right_data.clone()];
         let mut all_up_data = Vec::new();
         let mut all_done = true;
-        //
-        // for child in self.children.iter() {
-        //     let mut new_parsers = Vec::new();
-        //     let mut new_right_data = Vec::new();
-        //
-        //     for right_data in current_right_data.into_iter() {
-        //         let (parser, ParseResults { right_data_vec, up_data_vec, done }) = child.parser(right_data);
-        //         if !done {
-        //             new_parsers.push(parser);
-        //             all_done = false;
-        //         }
-        //         new_right_data.extend(right_data_vec);
-        //         all_up_data.extend(up_data_vec);
-        //     }
-        //
-        //     children.push((child.clone(), new_parsers));
-        //     current_right_data = new_right_data;
-        // }
-        let children = self.children.iter().map(|child| (child.clone(), Vec::new())).collect();
-        let parser = Parser::SeqParser(SeqParser { children });
 
-        for (combinator, parsers) in &mut self.children {
-            helper(c, combinator, parsers, &mut current_right_data, &mut all_up_data, &mut all_done);
+        for child in self.children.iter() {
+            let mut new_parsers = Vec::new();
+            let mut new_right_data = Vec::new();
+
+            for right_data in current_right_data.into_iter() {
+                let (parser, ParseResults { right_data_vec, up_data_vec, done }) = child.parser(right_data);
+                if !done {
+                    new_parsers.push(parser);
+                    all_done = false;
+                }
+                new_right_data.extend(right_data_vec);
+                all_up_data.extend(up_data_vec);
+            }
+
+            children.push((child.clone(), new_parsers));
+            current_right_data = new_right_data;
         }
+
+        let parser = Parser::SeqParser(SeqParser { children });
 
         let parse_results = ParseResults {
             right_data_vec: current_right_data,
@@ -53,30 +49,6 @@ impl CombinatorTrait for Seq {
     }
 }
 
-fn helper(c: u8, combinator: &Combinator, parsers: &mut Vec<Parser>, current_right_data: &mut Vec<RightData>, all_up_data: &mut Vec<UpData>, all_done: &mut bool) {
-    let mut next_right_data = Vec::new();
-
-    parsers.retain_mut(|mut parser| {
-        let ParseResults { right_data_vec, up_data_vec, done } = parser.step(c);
-        if !done {
-            *all_done = false;
-        }
-        next_right_data.extend(right_data_vec);
-        all_up_data.extend(up_data_vec);
-        !done
-    });
-
-    for right_data in current_right_data.drain(..) {
-        let (parser, ParseResults { right_data_vec, up_data_vec, done }) = combinator.parser(right_data);
-        if !done {
-            parsers.push(parser);
-            *all_done = false;
-        }
-        next_right_data.extend(right_data_vec);
-        all_up_data.extend(up_data_vec);
-    }
-}
-
 impl ParserTrait for SeqParser {
     fn step(&mut self, c: u8) -> ParseResults {
         let mut current_right_data = vec![];
@@ -84,7 +56,29 @@ impl ParserTrait for SeqParser {
         let mut all_done = true;
 
         for (combinator, parsers) in &mut self.children {
-            helper(c, combinator, parsers, &mut current_right_data, &mut all_up_data, &mut all_done);
+            let mut next_right_data = Vec::new();
+
+            parsers.retain_mut(|mut parser| {
+                let ParseResults { right_data_vec, up_data_vec, done } = parser.step(c);
+                if !done {
+                    all_done = false;
+                }
+                next_right_data.extend(right_data_vec);
+                all_up_data.extend(up_data_vec);
+                !done
+            });
+
+            for right_data in current_right_data.into_iter() {
+                let (parser, ParseResults { right_data_vec, up_data_vec, done }) = combinator.parser(right_data);
+                if !done {
+                    parsers.push(parser);
+                    all_done = false;
+                }
+                next_right_data.extend(right_data_vec);
+                all_up_data.extend(up_data_vec);
+            }
+
+            current_right_data = next_right_data;
         }
 
         ParseResults {
