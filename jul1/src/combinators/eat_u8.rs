59d3f91888eae5ff499e45d98d6ea9ec3ed649c6
IsaacBreen <mail@isaacbreen.com>
2024-07-31 14:51:28 +0800

    simpler eat u8

--- jul1/src/combinators/eat_u8.rs
+++ jul1/src/combinators/eat_u8.rs
@@ -47,20 +47,8 @@ impl ParserTrait for EatU8Parser {
         }
     }
 
-    fn steps(&mut self, bytes: &[u8]) -> ParseResults {  
-        let mut right_data_vec = Vec::new();  
-        for i in 0..bytes.len() {  
-            let ParseResults { right_data_vec: mut new_right_data_vec, up_data_vec, done } = self.step(bytes[i]);  
-            right_data_vec.append(&mut new_right_data_vec);  
-            if done || i == bytes.len() - 1 {  
-                return ParseResults {  
-                    right_data_vec,  
-                    up_data_vec,  
-                    done,  
-                };  
-            }  
-        }  
-        unreachable!();  
+    fn steps(&mut self, bytes: &[u8]) -> ParseResults {
+        self.step(bytes[0])
     }
 }
 
