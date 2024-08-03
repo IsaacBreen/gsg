2bcda0bf40fa1283656a1bf4e614015d58f8f8c6
IsaacBreen <mail@isaacbreen.com>
2024-08-03 22:00:27 +0800

    Revert "shorter"
    This reverts commit 159e90402579ad65dfcb461c231bf6fe8745b4c2.

--- jul1/src/combinators/repeat1.rs
+++ jul1/src/combinators/repeat1.rs
@@ -1,8 +1,8 @@
 use std::rc::Rc;
+
 use crate::{Combinator, CombinatorTrait, opt_greedy, Parser, ParseResults, ParserTrait, Squash, U8Set};
 use crate::combinators::derived::opt;
 use crate::parse_state::RightData;
-
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Repeat1 {
     pub(crate) a: Rc<Combinator>,
@@ -19,51 +19,58 @@ pub struct Repeat1Parser {
 
 impl CombinatorTrait for Repeat1 {
     fn parse(&self, right_data: RightData, bytes: &[u8]) -> (Parser, ParseResults) {
-        let mut right_data_as = Vec::new();
-        let mut new_parsers = Vec::new();
+        // Not done -> automatically passes
+        // Not greedy -> automatically passes
+        // Greedy and done -> only passes if a new parser called on right data doesn't have a right data without lookaheads (we can call this a dominating result).
+        let mut right_data_as = vec![];
+        let mut new_parsers = vec![];
         let mut prev_parsers_all_done = true;
 
-        let process_parse_results = |parse_results: ParseResults, new_parsers: &mut Vec<Parser>, right_data_as: &mut Vec<RightData>| {
-            right_data_as.extend(parse_results.right_data_vec);
-            if !parse_results.done {
-                new_parsers.push(parse_results.parser);
-            }
-            parse_results.done
-        };
-
         let (a, parse_results) = self.a.parse(right_data.clone(), bytes);
-        prev_parsers_all_done &= process_parse_results(parse_results, &mut new_parsers, &mut right_data_as);
+        right_data_as.extend(parse_results.right_data_vec);
+        if !parse_results.done {
+            new_parsers.push(a);
+        }
+        prev_parsers_all_done &= parse_results.done;
 
-        while !right_data_as.is_empty() {
-            let current_new_right_data = std::mem::take(&mut right_data_as);
+        let mut new_right_data = right_data_as.clone();
+        while new_right_data.len() > 0 {
+            let current_new_right_data = new_right_data;
+            new_right_data = vec![];
             let mut current_parsers_all_done = true;
-
             for right_data_a in current_new_right_data {
                 let offset = right_data_a.position - right_data.position;
                 let (a, parse_results) = self.a.parse(right_data_a, &bytes[offset..]);
-
                 if self.greedy && prev_parsers_all_done && !parse_results.right_data_vec.is_empty() {
-                    right_data_as.clear();
+                    // Clear all previous right and up data
+                    new_right_data.clear();
                 }
-
-                current_parsers_all_done &= process_parse_results(parse_results, &mut new_parsers, &mut right_data_as);
+                new_right_data.extend(parse_results.right_data_vec);
+                if !parse_results.done {
+                    new_parsers.push(a);
+                }
+                current_parsers_all_done &= parse_results.done;
             }
-
+            right_data_as.extend(new_right_data.clone());
             prev_parsers_all_done &= current_parsers_all_done;
         }
 
         right_data_as.squash();
 
+        let position = right_data.position + bytes.len();
+
+        let done = new_parsers.is_empty();
+
         (
             Parser::Repeat1Parser(Repeat1Parser {
                 a: self.a.clone(),
                 a_parsers: new_parsers,
-                position: right_data.position + bytes.len(),
+                position,
                 greedy: self.greedy
             }),
             ParseResults {
                 right_data_vec: right_data_as,
-                done: new_parsers.is_empty(),
+                done,
             }
         )
     }
@@ -71,36 +78,46 @@ impl CombinatorTrait for Repeat1 {
 
 impl ParserTrait for Repeat1Parser {
     fn get_u8set(&self) -> U8Set {
-        self.a_parsers.iter().fold(U8Set::none(), |acc, p| acc | p.get_u8set())
+        if self.a_parsers.is_empty() {
+            U8Set::none()
+        } else {
+            self.a_parsers.iter().map(|p| p.get_u8set()).reduce(|acc, e| acc | e).unwrap_or(U8Set::none())
+        }
     }
 
     fn parse(&mut self, bytes: &[u8]) -> ParseResults {
-        let mut right_data_as = Vec::new();
-        let mut new_parsers = Vec::new();
+        let mut right_data_as = vec![];
+        let mut new_parsers = vec![];
 
         for mut a_parser in self.a_parsers.drain(..) {
             let parse_results = a_parser.parse(bytes);
             if !parse_results.done {
                 new_parsers.push(a_parser);
             }
+            let discard_rest = self.greedy && parse_results.succeeds_tentatively();
             right_data_as.extend(parse_results.right_data_vec);
-            if self.greedy && parse_results.succeeds_tentatively() {
+            if discard_rest {
                 break;
             }
         }
 
         right_data_as.squash();
 
-        for right_data_a in right_data_as.clone() {
+        let mut i = 0;
+        while i < right_data_as.len() {
+            let right_data_a = right_data_as[i].clone();
             let offset = right_data_a.position - self.position;
-            let (a_parser, parse_results) = self.a.parse(right_data_a, &bytes[offset..]);
-            right_data_as.extend(parse_results.right_data_vec);
-            if !parse_results.done {
+            let (mut a_parser, ParseResults { right_data_vec: right_data_a, mut done }) = self.a.parse(right_data_a, &bytes[offset..]);
+            // todo: ??
+            right_data_as.extend(right_data_a);
+            if !done {
                 new_parsers.push(a_parser);
             }
+            i += 1;
         }
 
         self.a_parsers = new_parsers;
+
         self.position += bytes.len();
 
         ParseResults {
@@ -111,11 +128,17 @@ impl ParserTrait for Repeat1Parser {
 }
 
 pub fn repeat1(a: impl Into<Combinator>) -> Repeat1 {
-    Repeat1 { a: Rc::new(a.into()), greedy: false }
+    Repeat1 {
+        a: Rc::new(a.into()),
+        greedy: false,
+    }
 }
 
 pub fn repeat1_greedy(a: impl Into<Combinator>) -> Repeat1 {
-    Repeat1 { a: Rc::new(a.into()), greedy: true }
+    Repeat1 {
+        a: Rc::new(a.into()),
+        greedy: true,
+    }
 }
 
 pub fn repeat0(a: impl Into<Combinator>) -> Combinator {
