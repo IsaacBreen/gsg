eb6d3e5346998e401a8c5e4b1a1fec37cee762a8
IsaacBreen <mail@isaacbreen.com>
2024-08-04 12:19:36 +0800

    simplify eat_string

--- jul1/src/combinators/eat_string.rs
+++ jul1/src/combinators/eat_string.rs
@@ -1,5 +1,5 @@
 use crate::{Combinator, CombinatorTrait, Parser, ParseResults, ParserTrait, U8Set};
-use crate::parse_state::{RightData};
+use crate::parse_state::RightData;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct EatString {
@@ -20,7 +20,6 @@ pub struct EatStringParser {
 }
 
 impl CombinatorTrait for EatString {
-
     fn parse(&self, right_data: RightData, bytes: &[u8]) -> (Parser, ParseResults) {
         let mut parser = EatStringParser {
             string: self.string.clone(),
@@ -34,11 +33,7 @@ impl CombinatorTrait for EatString {
 
 impl ParserTrait for EatStringParser {
     fn get_u8set(&self) -> U8Set {
-        if self.index < self.string.len() {
-            U8Set::from_byte(self.string[self.index])
-        } else {
-            U8Set::none()
-        }
+        self.string.get(self.index).map_or(U8Set::none(), |&b| U8Set::from_byte(b))
     }
 
     fn parse(&mut self, bytes: &[u8]) -> ParseResults {
@@ -50,54 +45,38 @@ impl ParserTrait for EatStringParser {
         let mut done = false;
 
         for &byte in bytes {
-            if self.index < self.string.len() {
-                if self.string[self.index] == byte {
-                    self.index += 1;
-                    if self.index == self.string.len() {
-                        if let Some(mut right_data) = self.right_data.take() {
-                            right_data.position += self.string.len();
-                            right_data_vec.push(right_data);
-                            done = true;
-                            break;
-                        } else {
-                            panic!("EatStringParser already consumed");
-                        }
-                    } else {
-                        self.right_data.take();
+            if self.index >= self.string.len() {
+                break;
+            }
+
+            if self.string[self.index] == byte {
+                self.index += 1;
+                if self.index == self.string.len() {
+                    if let Some(mut right_data) = self.right_data.take() {
+                        right_data.position += self.string.len();
+                        right_data_vec.push(right_data);
                         done = true;
                         break;
                     }
-                } else {
-                    self.index = self.string.len();
-                    done = true;
-                    break;
                 }
             } else {
-                panic!("EatStringParser already consumed");
+                self.index = self.string.len();
+                break;
             }
         }
 
-        ParseResults {
-            right_data_vec,
-            done,
-        }
+        ParseResults { right_data_vec, done }
     }
 }
 
 pub fn eat_string(string: &str) -> EatString {
-    EatString {
-        string: string.as_bytes().to_vec(),
-    }
+    EatString { string: string.as_bytes().to_vec() }
 }
 
 pub fn eat_bytes(bytes: &[u8]) -> EatString {
-    EatString {
-        string: bytes.to_vec()
-    }
+    EatString { string: bytes.to_vec() }
 }
 
 pub fn eat(string: impl Into<String>) -> EatString {
-    EatString {
-        string: string.into().as_bytes().to_vec(),
-    }
+    EatString { string: string.into().into_bytes() }
 }
