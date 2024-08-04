c30174bb8258db0c59c4179453fb7d7529e980a1
IsaacBreen <mail@isaacbreen.com>
2024-08-04 12:08:35 +0800

    fix unicode

--- jul1/src/unicode.rs
+++ jul1/src/unicode.rs
@@ -4,11 +4,13 @@ use crate::unicode_categories::{char_ranges_in_general_category, GeneralCategory
 pub fn get_unicode_general_category_bytestrings(general_category: GeneralCategory) -> Vec<Vec<u8>> {
     let mut result = Vec::new();
 
-    for (start, end) in char_ranges_in_general_category(general_category) {
-        let utf8_bytes = start.to_string().as_bytes().to_vec();
-        result.push(utf8_bytes);
-        let utf8_bytes = end.to_string().as_bytes().to_vec();
-        result.push(utf8_bytes);
+    for (start, end) in char_ranges_in_general_category(general_category).iter().cloned() {
+        for c in start..=end {
+            let byte_len = c.len_utf8();
+            let mut bytes = vec![0; byte_len];
+            c.encode_utf8(&mut bytes);
+            result.push(bytes.to_vec());
+        }
     }
 
     result
