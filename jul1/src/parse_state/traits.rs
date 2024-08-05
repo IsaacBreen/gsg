c20b07b0a92d588b7556ee528f679894e8be60fe
IsaacBreen <mail@isaacbreen.com>
2024-08-05 12:40:28 +0800

    right data squasher

--- jul1/src/parse_state/traits.rs
+++ jul1/src/parse_state/traits.rs
@@ -14,20 +14,9 @@ impl Squash for Vec<RightData> {
     type Output = Vec<RightData>;
     fn squashed(self) -> Self::Output {
         if self.len() > SQUASH_THRESHOLD {
-            let mut decomposed: HashMap<RightData, LookaheadData> = HashMap::new();
-            for mut right_data in self {
-                let lookahead_data = std::mem::take(&mut right_data.lookahead_data);
-                let mut existing_lookahead_data = decomposed.entry(right_data).or_default();
-                // TODO: In general, all the lookaheads needs to be satisfied, i.e. it's an AND operation between Vecs of lookaheads. But this implies OR.
-                existing_lookahead_data.partial_lookaheads.extend(lookahead_data.partial_lookaheads);
-                existing_lookahead_data.has_omitted_partial_lookaheads &= lookahead_data.has_omitted_partial_lookaheads;
-            }
-            let mut result = vec![];
-            for (mut right_data, lookahead_data) in decomposed {
-                right_data.lookahead_data = lookahead_data;
-                result.push(right_data);
-            }
-            result
+            let mut squasher = RightDataSquasher::new();
+            squasher.extend(self);
+            squasher.finish()
         } else {
             self
         }
@@ -49,5 +38,42 @@ impl Squash for ParseResults {
     }
     fn squash(&mut self) {
         *self = self.clone().squashed();
+    }
+}
+
+pub struct RightDataSquasher {
+    decomposed: HashMap<RightData, LookaheadData>,
+}
+
+impl RightDataSquasher {
+    pub fn new() -> Self {
+        Self {
+            decomposed: HashMap::new(),
+        }
+    }
+}
+
+impl RightDataSquasher {
+    pub fn push(&mut self, right_data: RightData) {
+        let lookahead_data = std::mem::take(&mut right_data.lookahead_data);
+        let mut existing_lookahead_data = self.decomposed.entry(right_data).or_default();
+        // TODO: In general, all the lookaheads needs to be satisfied, i.e. it's an AND operation between Vecs of lookaheads. But this implies OR.
+        existing_lookahead_data.partial_lookaheads.extend(lookahead_data.partial_lookaheads);
+        existing_lookahead_data.has_omitted_partial_lookaheads &= lookahead_data.has_omitted_partial_lookaheads;
+    }
+
+    pub fn extend(&mut self, right_data_vec: Vec<RightData>) {
+        for right_data in right_data_vec {
+            self.push(right_data);
+        }
+    }
+
+    pub fn finish(self) -> Vec<RightData> {
+        let mut result = vec![];
+        for (mut right_data, lookahead_data) in self.decomposed {
+            right_data.lookahead_data = lookahead_data;
+            result.push(right_data);
+        }
+        result
     }
 }
