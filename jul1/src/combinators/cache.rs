1cc984f569f4e4253e331a4e045ac6c8ac800503
IsaacBreen <mail@isaacbreen.com>
2024-08-05 21:32:48 +0800

    hash ptr

--- jul1/src/combinators/cache.rs
+++ jul1/src/combinators/cache.rs
@@ -49,7 +49,8 @@ pub struct CachedParser {
 
 impl Hash for CachedParser {
     fn hash<H: Hasher>(&self, state: &mut H) {
-        // self.entry.borrow().hash(state);
+        // Hash the pointer
+        std::ptr::hash(self.entry.as_ref() as *const RefCell<CacheEntry>, state);
     }
 }
 
