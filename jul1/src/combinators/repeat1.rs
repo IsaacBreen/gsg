a0b8fcf5c262d9ce57c57cc864211fdecfa6d152
IsaacBreen <mail@isaacbreen.com>
2024-08-05 17:26:27 +0800

    fix

--- jul1/src/combinators/repeat1.rs
+++ jul1/src/combinators/repeat1.rs
@@ -1,7 +1,7 @@
 use std::rc::Rc;
 
 use crate::{Combinator, CombinatorTrait, opt_greedy, Parser, ParseResults, ParserTrait, profile_internal, Squash, U8Set};
-use crate::combinators::derived::opt;
+use crate::opt;
 use crate::parse_state::RightData;
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Repeat1 {
