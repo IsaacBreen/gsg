d9a3971623ea96f605f67546f49dd2e145753635
IsaacBreen <mail@isaacbreen.com>
2024-08-06 23:16:30 +0800

    fix unnecessary has_omitted_partial_lookaheads set

--- jul1/src/combinators/negative_lookahead.rs
+++ jul1/src/combinators/negative_lookahead.rs
@@ -40,9 +40,11 @@ impl CombinatorTrait for ExcludeBytestrings {
             }
             true
         });
-        for right_data in parse_results.right_data_vec.iter_mut() {
-            Rc::make_mut(&mut right_data.right_data_inner).lookahead_data.has_omitted_partial_lookaheads = true;
-        }
+        if !bytestrings_to_exclude.is_empty() {
+            for right_data in parse_results.right_data_vec.iter_mut() {
+                Rc::make_mut(&mut right_data.right_data_inner).lookahead_data.has_omitted_partial_lookaheads = true;
+            }
+            }
         (Parser::ExcludeBytestringsParser(ExcludeBytestringsParser {
             inner: Box::new(inner),
             bytestrings_to_exclude,
@@ -68,8 +70,10 @@ impl ParserTrait for ExcludeBytestringsParser {
             }
             true
         });
-        for right_data in parse_results.right_data_vec.iter_mut() {
-            Rc::make_mut(&mut right_data.right_data_inner).lookahead_data.has_omitted_partial_lookaheads = true;
+        if !self.bytestrings_to_exclude.is_empty() {
+            for right_data in parse_results.right_data_vec.iter_mut() {
+                Rc::make_mut(&mut right_data.right_data_inner).lookahead_data.has_omitted_partial_lookaheads = true;
+            }
         }
         self.position += bytes.len();
         parse_results
