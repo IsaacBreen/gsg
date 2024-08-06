4a55bc679e4bdd4385fc36edbcd34d5f21c1fbb6
IsaacBreen <mail@isaacbreen.com>
2024-08-06 16:15:44 +0800

    fix eat_u8 returning done: false when it actually *is* done.

--- jul1/src/combinators/eat_u8.rs
+++ jul1/src/combinators/eat_u8.rs
@@ -14,17 +14,12 @@ pub struct EatU8Parser {
 
 impl CombinatorTrait for EatU8 {
     fn parse(&self, right_data: RightData, bytes: &[u8]) -> (Parser, ParseResults) {
-        let parser1 = EatU8Parser {
+        let mut parser = EatU8Parser {
             u8set: self.u8set.clone(),
             right_data: Some(right_data),
         };
-        let (mut parser, mut parse_results0) = (Parser::EatU8Parser(parser1), ParseResults {
-            right_data_vec: crate::VecY::new(),
-            done: false,
-        });
-        let parse_results1 = parser.parse(bytes);
-        parse_results0.combine_seq(parse_results1);
-        (parser, parse_results0)
+        let parse_results = parser.parse(bytes);
+        (Parser::EatU8Parser(parser), parse_results)
     }
 }
 
