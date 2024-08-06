97fe495a196eb026dbc9be2605e8bb8f4107f214
IsaacBreen <mail@isaacbreen.com>
2024-08-06 16:23:38 +0800

    prints

--- jul1/src/combinator.rs
+++ jul1/src/combinator.rs
@@ -160,8 +160,8 @@ pub trait ParserTrait {
 impl CombinatorTrait for Combinator {
     fn parse(&self, right_data: RightData, bytes: &[u8]) -> (Parser, ParseResults) {
         let (parser, parse_results) = match_combinator!(self, inner => inner.parse(right_data, bytes));
-        if !parse_results.done {
-            println!("Combinator {:?} did not consume all input", self);
+        if !parse_results.done && bytes.len() > 100 {
+            println!("Combinator {:?} did not consume all input. Positions: {:?}, bytes.len(): {}", self, parse_results.right_data_vec.iter().map(|x| x.position).collect::<Vec<_>>(), bytes.len());
         }
         (parser, parse_results)
     }
