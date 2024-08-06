a8e9e0a79e7a0d97eba0565e639b698ae098cc24
IsaacBreen <mail@isaacbreen.com>
2024-08-06 21:43:38 +0800

    unprofile

--- jul1/src/combinators/profile.rs
+++ jul1/src/combinators/profile.rs
@@ -56,9 +56,9 @@ impl ProfileDataInner {
 #[macro_export]
 macro_rules! profile {
     ($tag:expr, $body:expr) => {{
-        $crate::ProfileDataInner::push_tag($tag.to_string());
+        // $crate::ProfileDataInner::push_tag($tag.to_string());
         let result = $body;
-        $crate::ProfileDataInner::pop_tag();
+        // $crate::ProfileDataInner::pop_tag();
         result
     }};
 }
@@ -66,9 +66,9 @@ macro_rules! profile {
 #[macro_export]
 macro_rules! profile_block {
     ($body:expr) => {{
-        $crate::ProfileDataInner::push_tag(format!("{}:{}", file!(), line!()));
+        // $crate::ProfileDataInner::push_tag(format!("{}:{}", file!(), line!()));
         let result = $body;
-        $crate::ProfileDataInner::pop_tag();
+        // $crate::ProfileDataInner::pop_tag();
         result
     }};
 }
