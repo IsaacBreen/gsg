56940c5d4562d7141b19fa815736fac0e4dc8f75
IsaacBreen <mail@isaacbreen.com>
2024-08-08 12:24:43 +0800

    cache deferred (broken)

--- jul1/src/combinators/deferred.rs
+++ jul1/src/combinators/deferred.rs
@@ -1,22 +1,30 @@
 use std::fmt::{Debug, Formatter};
 use std::hash::{Hash, Hasher};
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::HashMap;
 use crate::*;
 use crate::VecX;
+
+thread_local! {
+    static COMBINATOR_CACHE: RefCell<HashMap<usize, Rc<Combinator>>> = RefCell::new(HashMap::new());
+}
 
 #[derive(Clone)]
 pub struct Deferred {
     pub(crate) f: Rc<dyn Fn() -> Combinator>,
+    cache_key: usize,
 }
 
 impl Hash for Deferred {
     fn hash<H: Hasher>(&self, state: &mut H) {
-        std::ptr::hash(self.f.as_ref() as *const dyn Fn() -> Combinator, state);
+        self.cache_key.hash(state);
     }
 }
 
 impl PartialEq for Deferred {
     fn eq(&self, other: &Self) -> bool {
-        Rc::ptr_eq(&self.f, &other.f)
+        self.cache_key == other.cache_key
     }
 }
 
@@ -24,22 +32,31 @@ impl Eq for Deferred {}
 
 impl Debug for Deferred {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Deferred")
+        write!(f, "Deferred({})", self.cache_key)
     }
 }
 
 impl CombinatorTrait for Deferred {
     fn parse(&self, right_data: RightData, bytes: &[u8]) -> (Parser, ParseResults) {
-        let combinator = self.f.as_ref()();
+        let combinator = COMBINATOR_CACHE.with(|cache| {
+            let mut cache = cache.borrow_mut();
+            cache.entry(self.cache_key)
+                .or_insert_with(|| Rc::new((self.f)()))
+                .clone()
+        });
         combinator.parse(right_data, bytes)
     }
 }
 
 pub fn deferred(f: impl Fn() -> Combinator + 'static) -> Combinator {
-    Deferred { f: Rc::new(f) }.into()
+    let cache_key = &f as *const _ as usize;
+    Deferred { f: Rc::new(f), cache_key }.into()
 }
 
-impl<T> From<&'static T> for Combinator where T: Fn() -> Combinator {
+impl<T> From<&'static T> for Combinator
+where
+    T: Fn() -> Combinator
+{
     fn from(value: &'static T) -> Self {
         deferred(value as &dyn Fn() -> Combinator)
     }
