95bf94be59ab57429757cfefde5196e585d019b2
IsaacBreen <mail@isaacbreen.com>
2024-08-09 11:53:01 +0800

    compile deferred

--- jul1/src/compiler.rs
+++ jul1/src/compiler.rs
@@ -1,7 +1,13 @@
 use crate::*;
 
 impl Combinator {
-    pub fn compile(self) -> Combinator {
-        self
+    pub fn compile(mut self) -> Combinator {
+        self.map_recursive_preorder(|combinator| {
+            match combinator {
+                // Construct the deferred combinator
+                Combinator::Deferred(Deferred { f }) => f(),
+                _ => combinator
+            }
+        })
     }
 }
