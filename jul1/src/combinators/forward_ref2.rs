817b32b7a6bc30fdac5e23fade2da658f6d8ca0e
IsaacBreen <mail@isaacbreen.com>
2024-08-09 14:38:40 +0800

    hmmm

--- jul1/src/combinators/forward_ref2.rs
+++ jul1/src/combinators/forward_ref2.rs
@@ -1,31 +1,71 @@
 use std::cell::RefCell;
+use std::hash::{Hash, Hasher};
+use std::ptr::NonNull;
 use crate::*;
 
-#[derive(Debug, Clone)]
-pub struct ForwardRef2<'a> {
-    b: Rc<RefCell<Option<&'a Combinator>>>,
+#[derive(Debug, Clone, Eq)]
+pub struct ForwardRef2 {
+    b: Rc<RefCell<Option<&'static Combinator>>>,
 }
 
-impl<'a> ForwardRef2<'a> {
-    pub fn set(&mut self, a: impl Into<Combinator>) -> Symbol {
-        let a: Rc<Combinator> = a.into().into();
+impl PartialEq for ForwardRef2 {
+    fn eq(&self, other: &Self) -> bool {
+        std::ptr::eq(self, other)
+    }
+}
+
+impl Hash for ForwardRef2 {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        std::ptr::hash(self, state);
+    }
+}
+
+impl ForwardRef2 {
+    pub fn set(&mut self, a: Combinator) {
         *self.b.borrow_mut() = Some(&a);
-        Symbol { value: a }
     }
 }
 
-impl<'a> CombinatorTrait for ForwardRef2<'a> {
+impl CombinatorTrait for ForwardRef2 {
     fn parse(&self, right_data: RightData, bytes: &[u8]) -> (Parser, ParseResults) {
-        self.b.borrow().as_ref().unwrap().parse(right_data, bytes)
+        let binding = self.b.borrow();
+        let a = binding.as_ref().unwrap();
+        a.parse(right_data, bytes)
     }
 }
 
-impl<'a> From<&'a ForwardRef2<'a>> for Combinator {
-    fn from(value: &'a ForwardRef2<'a>) -> Self {
+impl From<&ForwardRef2> for Combinator {
+    fn from(value: &ForwardRef2) -> Self {
         Combinator::ForwardRef2(value.clone())
     }
 }
 
-pub fn forward_ref2<'a>() -> ForwardRef2<'a> {
+pub fn forward_ref2() -> ForwardRef2 {
     ForwardRef2 { b: Rc::new(RefCell::new(None)) }
+}
+
+#[test]
+fn test_forward_ref2() {
+    // should fail to compile :
+    // error[E0597]: `a` does not live long enough
+    //   --> src/combinators/forward_ref2.rs:52:15
+    //    |
+    // 51 |         let a: Combinator = eps().into();
+    //    |             - binding `a` declared here
+    // 52 |         f.set(&a);
+    //    |         ------^^-
+    //    |         |     |
+    //    |         |     borrowed value does not live long enough
+    //    |         argument requires that `a` is borrowed for `'static`
+    // 53 |         f
+    // 54 |     }
+    //    |     - `a` dropped here while still borrowed
+    //
+    fn make() -> Combinator {
+        let mut f = forward_ref2();
+        let a: Combinator = eps().into();
+        let b = seq!(eat('b'), &f);
+        f.set(&a);
+        b
+    }
 }
