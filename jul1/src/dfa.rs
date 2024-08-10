611c846b4a25e8d580555036ecfe60a721075727
IsaacBreen <mail@isaacbreen.com>
2024-08-10 23:26:25 +0800

    add dfa.rs

---
+++ jul1/src/dfa.rs
@@ -0,0 +1,45 @@
+use std::collections::HashMap;
+use crate::fast_combinator::FastParserResult;
+use crate::U8Set;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct DFAState {
+    is_accepting: bool,
+    transitions: HashMap<u8, usize>, // byte -> next state index
+}
+
+#[derive(Debug, Clone)]
+pub struct DFA {
+    states: Vec<DFAState>,
+    start_state: usize,
+}
+
+impl DFA {
+    pub fn new(start_state: usize, states: Vec<DFAState>) -> Self {
+        DFA { states, start_state }
+    }
+
+    pub fn parse(&self, bytes: &[u8]) -> FastParserResult {
+        let mut current_state = self.start_state;
+        let mut bytes_consumed = 0;
+
+        for &byte in bytes {
+            if let Some(next_state) = self.states[current_state].transitions.get(&byte) {
+                current_state = *next_state;
+                bytes_consumed += 1;
+            } else {
+                return if self.states[current_state].is_accepting {
+                    FastParserResult::Success(bytes_consumed)
+                } else {
+                    FastParserResult::Failure
+                };
+            }
+        }
+
+        if self.states[current_state].is_accepting {
+            FastParserResult::Success(bytes_consumed)
+        } else {
+            FastParserResult::Incomplete
+        }
+    }
+}
