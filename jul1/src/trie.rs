dc889cefe0c6e77f36ee8fa9ad297cbf22f9c288
IsaacBreen <mail@isaacbreen.com>
2024-08-11 13:14:46 +0800

    add some more

--- jul1/src/trie.rs
+++ jul1/src/trie.rs
@@ -85,6 +85,25 @@ impl TrieNode {
         (current_node, bytes.len(), FinishReason::EndOfInput)
     }
 
+    pub fn last(&self, bytes: &[u8]) -> (&TrieNode, usize, FinishReason) {
+        let mut current_node = self;
+        for (i, &byte) in bytes.iter().enumerate() {
+            if current_node.valid_bytes.contains(byte) {
+                let child_index = current_node.valid_bytes.bitset.count_bits_before(byte) as usize;
+                current_node = &current_node.children[child_index];
+            } else if current_node.is_end {
+                return (current_node, i, FinishReason::Success);
+            } else {
+                return (current_node, i, FinishReason::Failure);
+            }
+        }
+        if current_node.is_end {
+            (current_node, bytes.len(), FinishReason::Success)
+        } else {
+            (current_node, bytes.len(), FinishReason::EndOfInput)
+        }
+    }
+
     pub fn all_next(&self, bytes: &[u8]) -> (Vec<(&TrieNode, usize)>, (&TrieNode, usize, FinishReason)) {
         let mut results = vec![];
         let mut current_node = self;
@@ -102,6 +121,16 @@ impl TrieNode {
                 }
             }
         }
+    }
+
+    pub fn contains_full(&self, bytes: &[u8]) -> bool {
+        let (node, bytes_consumed, finish_reason) = self.last(bytes);
+        finish_reason == FinishReason::Success
+    }
+
+    pub fn starts_with(&self, bytes: &[u8]) -> bool {
+        let (node, bytes_consumed, finish_reason) = self.last(bytes);
+        finish_reason == FinishReason::Success && bytes_consumed == bytes.len()
     }
 
     pub fn is_end(&self) -> bool {
