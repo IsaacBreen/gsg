d792de419cc43e6293b258738bbd1e82a690fb87
IsaacBreen <mail@isaacbreen.com>
2024-08-11 13:03:59 +0800

    hm

--- jul1/src/python/python_tokenizer.rs
+++ jul1/src/python/python_tokenizer.rs
@@ -300,12 +300,14 @@ pub fn id_continue_bytestrings() -> Vec<Vec<u8>> {
 }
 
 pub fn id_start_fast() -> FastParser {
-    eat_bytestring_choice_fast(id_start_bytestrings())
+    // eat_bytestring_choice_fast(id_start_bytestrings())
+    eat_bytestring_choice_fast(vec![b'_', b'a', b'b', b'c', b'd', b'e', b'f', b'g', b'h', b'i', b'j', b'k', b'l', b'm', b'n', b'o', b'p', b'q', b'r', b's', b't', b'u', b'v', b'w', b'x', b'y', b'z', b'A', b'B', b'C', b'D', b'E', b'F', b'G', b'H', b'I', b'J', b'K', b'L', b'M', b'N', b'O', b'P', b'Q', b'R', b'S', b'T', b'U', b'V', b'W', b'X', b'Y', b'Z'].into_iter().map(|byte| vec![byte]).collect())
     // eat_char_choice_fast("_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 }
 
 pub fn id_continue_fast() -> FastParser {
-    eat_bytestring_choice_fast(id_continue_bytestrings())
+    // eat_bytestring_choice_fast(id_continue_bytestrings())
+    eat_bytestring_choice_fast(vec![b'_', b'a', b'b', b'c', b'd', b'e', b'f', b'g', b'h', b'i', b'j', b'k', b'l', b'm', b'n', b'o', b'p', b'q', b'r', b's', b't', b'u', b'v', b'w', b'x', b'y', b'z', b'A', b'B', b'C', b'D', b'E', b'F', b'G', b'H', b'I', b'J', b'K', b'L', b'M', b'N', b'O', b'P', b'Q', b'R', b'S', b'T', b'U', b'V', b'W', b'X', b'Y', b'Z', b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9'].into_iter().map(|byte| vec![byte]).collect())
     // eat_char_choice_fast("_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 }
 
