8404ecd95dfc752c0ac3fd5a1b7941f8b8bc2e97
IsaacBreen <mail@isaacbreen.com>
2024-08-11 15:39:22 +0800

    maybe faster

--- jul1/src/python/python_tokenizer.rs
+++ jul1/src/python/python_tokenizer.rs
@@ -92,13 +92,13 @@ pub fn whitespace() -> Combinator {
 
     return choice_greedy!(
         seq!(
+            check_right_data(|right_data| right_data.right_data_inner.scope_count == 0),
+            repeat1_fast(eat_string_choice_fast(&[" ", "\t", "\\\n"]))
+        ),
+        seq!(
             check_right_data(|right_data| right_data.right_data_inner.scope_count > 0),
             repeat1_fast(eat_string_choice_fast(&[" ", "\t", "\\\n", "\n", "\r"]))
         ),
-        seq!(
-            check_right_data(|right_data| right_data.right_data_inner.scope_count == 0),
-            repeat1_fast(eat_string_choice_fast(&[" ", "\t", "\\\n"]))
-        )
     );
 
     brute_force(|mut right_data, bytes| {
