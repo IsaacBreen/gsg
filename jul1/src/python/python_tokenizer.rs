a61a64ead874d01cfe56042953458f10f5db3e03
IsaacBreen <mail@isaacbreen.com>
2024-08-11 16:13:46 +0800

    try more

--- jul1/src/python/python_tokenizer.rs
+++ jul1/src/python/python_tokenizer.rs
@@ -138,8 +138,8 @@ pub fn WS() -> Combinator {
 }
 
 pub fn python_literal(s: &str) -> Combinator {
-    let increment_scope_count = |right_data: &mut RightData| { Rc::make_mut(&mut right_data.right_data_inner).scope_count += 1; Rc::make_mut(&mut right_data.right_data_inner).forbidden_consecutive_matches.prev_match_ids.clear(); true };
-    let decrement_scope_count = |right_data: &mut RightData| { Rc::make_mut(&mut right_data.right_data_inner).scope_count -= 1; Rc::make_mut(&mut right_data.right_data_inner).forbidden_consecutive_matches.prev_match_ids.clear(); true };
+    let increment_scope_count = |right_data: &mut RightData| { let mut right_data_inner = Rc::make_mut(&mut right_data.right_data_inner); right_data_inner.scope_count += 1; right_data_inner.forbidden_consecutive_matches.prev_match_ids.clear(); true };
+    let decrement_scope_count = |right_data: &mut RightData| { let mut right_data_inner = Rc::make_mut(&mut right_data.right_data_inner); right_data_inner.scope_count -= 1; right_data_inner.forbidden_consecutive_matches.prev_match_ids.clear(); true };
 
     match s {
         "(" | "[" | "{" => seq!(eat_string(s), mutate_right_data(increment_scope_count)),
