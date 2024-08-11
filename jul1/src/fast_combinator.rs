c739d41b9a1f9b8c488a89f8c707d0ae4534f127
IsaacBreen <mail@isaacbreen.com>
2024-08-11 13:21:17 +0800

    fix

--- jul1/src/fast_combinator.rs
+++ jul1/src/fast_combinator.rs
@@ -85,7 +85,7 @@ impl FastParser {
             }
             FastParser::EatByteStringChoiceFast(root) => {
                 let (current_node, bytes_consumed, finish_reason) = root.next(bytes);
-                if root.all_next(bytes).0.is_empty() {
+                if !root.all_next(bytes).0.is_empty() {
                     let s = String::from_utf8_lossy(bytes);
                     panic!("Ambiguous parse: {:?}", s[..s.len().min(100)].to_string());
                 }
