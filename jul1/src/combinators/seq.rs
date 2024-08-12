18a007ca4625ac6ac8277edb5533c8f42d0eef14
IsaacBreen <mail@isaacbreen.com>
2024-08-12 14:32:20 +0800

    no profile

--- jul1/src/combinators/seq.rs
+++ jul1/src/combinators/seq.rs
@@ -114,7 +114,9 @@ impl ParserTrait for SeqParser {
         let mut parser_initialization_queue: BTreeMap<usize, RightDataSquasher> = BTreeMap::new();
 
         // Eliminate duplicate parsers
-        // self.parsers = std::mem::take(&mut self.parsers).into_iter().collect::<HashSet<_>>().into_iter().collect();
+        // if self.parsers.len() > 10 {
+        //     self.parsers = std::mem::take(&mut self.parsers).into_iter().collect::<HashSet<_>>().into_iter().collect();
+        // }
 
         profile!("SeqParser::parse part 1", {
         self.parsers.retain_mut(|(combinator_index, parser)| {
