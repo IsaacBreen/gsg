beca50ea9e3bd2e9e715c40415ae7a36adba3218
IsaacBreen <mail@isaacbreen.com>
2024-08-12 11:52:39 +0800

    comment

--- jul1/src/transpose.rs
+++ jul1/src/transpose.rs
@@ -4,6 +4,8 @@ impl Combinator {
     pub fn transpose(&mut self) -> Self {
         // Converts a combinator into a form where the outer expression is a choice between sequences that begin with a terminal.
         //
+        // Here, a 'terminal' is defined (loosely) as any combinator that is not a sequence or a choice.
+        //
         // Example transpositions:
         //
         // seq(seq(a, b, c), d, e)) =>
