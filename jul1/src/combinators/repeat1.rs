8abec14f6cdf2950b56da1d8f909ded18b6b75c9
IsaacBreen <mail@isaacbreen.com>
2024-08-13 15:31:11 +0800

    try speed up

--- jul1/src/combinators/repeat1.rs
+++ jul1/src/combinators/repeat1.rs
@@ -43,10 +43,16 @@ impl CombinatorTrait for Repeat1 {
         };
         let mut all_prev_succeeded_decisively = parse_results.succeeds_decisively();
         let mut right_data_vec = parse_results.right_data_vec;
+        let mut i = 0;
+        let mut i_parser = 0;
+        let mut i_start = 0;
+        let mut i_parser_start = 0;
 
-        let mut next_right_data = right_data_vec.clone();
-        while next_right_data.len() > 0 {
-            for new_right_data in std::mem::take(&mut next_right_data) {
+        while i < right_data_vec.len() {
+            let next_i = right_data_vec.len();
+            let next_i_parser = parsers.len();
+            for j in i..right_data_vec.len() {
+                let new_right_data = right_data_vec[j].clone();
                 let offset = new_right_data.right_data_inner.fields1.position - start_position;
                 let (parser, parse_results) = self.a.parse(new_right_data, &bytes[offset..]);
                 if !parse_results.done() {
@@ -54,23 +60,22 @@ impl CombinatorTrait for Repeat1 {
                 }
                 all_prev_succeeded_decisively &= parse_results.succeeds_decisively();
                 if self.greedy && all_prev_succeeded_decisively {
-                    right_data_vec.clear();
-                    parsers.clear();
+                    i_start = i;
+                    i_parser_start = i_parser;
                 }
-                // if !(self.greedy && parse_results.succeeds_decisively()) && parse_results.right_data_vec.len() > 0 && right_data_vec.len() > 0 {
-                //     println!("parse_results: {:?}", parse_results);
-                // }
-                next_right_data.extend(parse_results.right_data_vec);
+                right_data_vec.extend(parse_results.right_data_vec);
             }
-            if !right_data_vec.is_empty() && !next_right_data.is_empty() {
-                let end_pos = start_position + bytes.len();
-                let pos1 = right_data_vec[0].right_data_inner.fields1.position;
-                let pos2 = next_right_data[0].right_data_inner.fields1.position;
-                if end_pos < pos1 + 1000 || end_pos < pos2 + 1000 {
-                    right_data_vec.clear();
-                }
-            }
-            right_data_vec.extend(next_right_data.clone());
+            // if !right_data_vec.is_empty() && !next_right_data.is_empty() {
+            //     let end_pos = start_position + bytes.len();
+            //     let pos1 = right_data_vec[0].right_data_inner.fields1.position;
+            //     let pos2 = next_right_data[0].right_data_inner.fields1.position;
+            //     if end_pos < pos1 + 1000 || end_pos < pos2 + 1000 {
+            //         right_data_vec.clear();
+            //     }
+            // }
+            // right_data_vec.extend(next_right_data.clone());
+            i = next_i;
+            i_parser = next_i_parser;
         }
 
         let done = parsers.is_empty();
