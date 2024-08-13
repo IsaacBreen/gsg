a0c3ff96a9444840e6fa89cb60a85144fee1b793
IsaacBreen <mail@isaacbreen.com>
2024-08-13 13:54:54 +0800

    Revert "just use a hashmap"
    This reverts commit fd8c726af06464c0caa6f51300ae0227edcd0d4e.

--- jul1/src/combinators/cache.rs
+++ jul1/src/combinators/cache.rs
@@ -5,6 +5,7 @@ use std::num::NonZeroUsize;
 use std::rc::Rc;
 
 use derivative::Derivative;
+use caches::{DefaultHashBuilder, LRUCache};
 use crate::{Combinator, CombinatorTrait, Parser, ParseResults, ParserTrait, profile, profile_internal, RightData, Squash, U8Set};
 
 macro_rules! profile {
@@ -20,7 +21,7 @@ thread_local! {
 #[derive(Debug)]
 struct GlobalCache {
     new_parsers: HashMap<usize, HashMap<CacheKey, Rc<RefCell<CacheEntry>>>>,
-    pub(crate) entries: HashMap<usize, Vec<Rc<RefCell<CacheEntry>>>>,
+    pub(crate) entries: LRUCache<usize, Vec<Rc<RefCell<CacheEntry>>>, DefaultHashBuilder>,
     pub(crate) parse_id_counter: usize,
     pub(crate) parse_id: Option<usize>,
 }
@@ -29,7 +30,7 @@ impl GlobalCache {
     fn new() -> Self {
         Self {
             new_parsers: HashMap::new(),
-            entries: HashMap::new(),
+            entries: LRUCache::new(64),
             parse_id_counter: 0,
             parse_id: None,
         }
@@ -50,9 +51,7 @@ struct CacheKey {
 
 impl Hash for CacheKey {
     fn hash<H: Hasher>(&self, state: &mut H) {
-        // std::mem::discriminant(self.combinator.as_ref()).hash(state);
-        Rc::as_ptr(&self.combinator).hash(state);
-        // self.combinator.hash(state);
+        std::mem::discriminant(self.combinator.as_ref()).hash(state);
         self.right_data.hash(state);
     }
 }
@@ -60,7 +59,6 @@ impl Hash for CacheKey {
 impl PartialEq for CacheKey {
     fn eq(&self, other: &Self) -> bool {
         Rc::ptr_eq(&self.combinator, &other.combinator) && self.right_data == other.right_data
-        // self.combinator == other.combinator && self.right_data == other.right_data
     }
 }
 
@@ -111,7 +109,7 @@ impl CombinatorTrait for CacheContext {
             let parse_id = {
                 let mut global_cache = cache.borrow_mut();
                 let parse_id = global_cache.parse_id_counter;
-                global_cache.new_parsers.insert(parse_id, HashMap::new());
+                global_cache.new_parsers.insert(parse_id, LruCache::new(NonZeroUsize::new(64).unwrap()));
                 global_cache.entries.insert(parse_id, Vec::new());
                 global_cache.parse_id = Some(global_cache.parse_id_counter);
                 global_cache.parse_id_counter += 1;
@@ -187,7 +185,7 @@ impl CombinatorTrait for Cached {
 
             let mut global_cache = cache.borrow_mut();
             let parse_id = global_cache.parse_id.unwrap();
-            global_cache.new_parsers.get_mut(&parse_id).unwrap().insert(key, entry.clone());
+            global_cache.new_parsers.get_mut(&parse_id).unwrap().put(key, entry.clone());
             if !parse_results.done() {
                 global_cache.entries.get_mut(&parse_id).unwrap().push(entry.clone());
             }
