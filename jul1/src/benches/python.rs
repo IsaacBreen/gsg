13c31d675077aac8a9be8374d06b96df23f707ce
IsaacBreen <mail@isaacbreen.com>
2024-08-13 12:50:01 +0800

    test fast

--- jul1/src/benches/python.rs
+++ jul1/src/benches/python.rs
@@ -1,21 +1,11 @@
+use std::path::Path;
 use std::time::Instant;
 use jun3::python_file;
-use jun3::utils::assert_parses;
+use jun3::utils::{assert_parses, assert_parses_fast};
 
 fn main() {
+    let path = Path::new("src/tests/test_input.py");
+    let file = std::fs::read_to_string(path).unwrap();
     let combinator = python_file();
-
-    let test_cases = [
-        // ("Simple string", "x = 12\nx = 2\nx"),
-        ("dump_python_gram.py", include_str!("../python/dump_python_gram.py")),
-        // ("remove_left_recursion.py", include_str!("../python/remove_left_recursion.py")),
-        // ("test_input.py", include_str!("../tests/test_input.py")),
-    ];
-
-    for (name, content) in test_cases.iter() {
-        let start = Instant::now();
-        assert_parses(&combinator, content, name);
-        let duration = start.elapsed();
-        println!("{:<25} parsed in {:?}", name, duration);
-    }
+    assert_parses_fast(&combinator, &file);
 }
