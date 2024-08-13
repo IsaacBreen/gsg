7a408ab91f33007ad11df1b7a68744ffee697fed
IsaacBreen <mail@isaacbreen.com>
2024-08-13 15:35:23 +0800

    Revert "try speed up"
    This reverts commit 8abec14f6cdf2950b56da1d8f909ded18b6b75c9.

--- jul1/src/combinators/repeat1.rs
+++ jul1/src/combinators/repeat1.rs
@@ -43,16 +43,10 @@ impl CombinatorTrait for Repeat1 {
         };
         let mut all_prev_succeeded_decisively = parse_results.succeeds_decisively();
         let mut right_data_vec = parse_results.right_data_vec;
-        let mut i = 0;
-        let mut i_parser = 0;
-        let mut i_start = 0;
-        let mut i_parser_start = 0;
 
-        while i < right_data_vec.len() {
-            let next_i = right_data_vec.len();
-            let next_i_parser = parsers.len();
-            for j in i..right_data_vec.len() {
-                let new_right_data = right_data_vec[j].clone();
+        let mut next_right_data = right_data_vec.clone();
+        while next_right_data.len() > 0 {
+            for new_right_data in std::mem::take(&mut next_right_data) {
                 let offset = new_right_data.right_data_inner.fields1.position - start_position;
                 let (parser, parse_results) = self.a.parse(new_right_data, &bytes[offset..]);
                 if !parse_results.done() {
@@ -60,23 +54,23 @@ impl CombinatorTrait for Repeat1 {
                 }
                 all_prev_succeeded_decisively &= parse_results.succeeds_decisively();
                 if self.greedy && all_prev_succeeded_decisively {
-                    i_start = i;
-                    i_parser_start = i_parser;
+                    right_data_vec.clear();
+                    parsers.clear();
                 }
-                right_data_vec.extend(parse_results.right_data_vec);
-            }
-            // if !right_data_vec.is_empty() && !next_right_data.is_empty() {
-                // let end_pos = start_position + bytes.len();
-                // let pos1 = right_data_vec[0].right_data_inner.fields1.position;
-                // let pos2 = next_right_data[0].right_data_inner.fields1.position;
-                // if end_pos < pos1 + 1000 || end_pos < pos2 + 1000 {
-                //     right_data_vec.clear();
+                // if !(self.greedy && parse_results.succeeds_decisively()) && parse_results.right_data_vec.len() > 0 && right_data_vec.len() > 0 {
+                //     println!("parse_results: {:?}", parse_results);
                 // }
-            // }
-
-            // right_data_vec.extend(next_right_data.clone());
-            i = next_i;
-            i_parser = next_i_parser;
+                next_right_data.extend(parse_results.right_data_vec);
+            }
+            if !right_data_vec.is_empty() && !next_right_data.is_empty() {
+                let end_pos = start_position + bytes.len();
+                let pos1 = right_data_vec[0].right_data_inner.fields1.position;
+                let pos2 = next_right_data[0].right_data_inner.fields1.position;
+                if end_pos < pos1 + 1000 || end_pos < pos2 + 1000 {
+                    right_data_vec.clear();
+                }
+            }
+            right_data_vec.extend(next_right_data.clone());
         }
 
         let done = parsers.is_empty();
@@ -84,11 +78,11 @@ impl CombinatorTrait for Repeat1 {
         (
             Parser::Repeat1Parser(Repeat1Parser {
                 a: self.a.clone(),
-                a_parsers: parsers[i_parser..].to_vec(),
+                a_parsers: parsers,
                 position: start_position + bytes.len(),
                 greedy: self.greedy
             }),
-            ParseResults::new(right_data_vec[i..].to_vec(), done)
+            ParseResults::new(right_data_vec, done)
         )
     }
 }
