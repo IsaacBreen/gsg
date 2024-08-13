2693b3137dd4ff8c47dca9425389806c097df68c
IsaacBreen <mail@isaacbreen.com>
2024-08-13 13:19:27 +0800

    hmmm

--- jul1/src/benches/python.rs
+++ jul1/src/benches/python.rs
@@ -4,6 +4,7 @@ use jun3::python_file;
 use jun3::utils::{assert_parses, assert_parses_fast};
 
 fn main() {
+    println!("Running Python benchmarks...");
     let path = Path::new("src/tests/test_input.py");
     let file = std::fs::read_to_string(path).unwrap();
     let combinator = python_file();
