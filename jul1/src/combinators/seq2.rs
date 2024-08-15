9f4e268beef3d06beb5c44d0116bf1c2c8317848
IsaacBreen <mail@isaacbreen.com>
2024-08-15 18:03:33 +0800

    generalise to SeqN

--- jul1/src/combinators/seq2.rs
+++ jul1/src/combinators/seq2.rs
@@ -1,42 +1,65 @@
+use std::rc::Rc;
+use std::collections::BTreeMap;
+use crate::{Combinator, CombinatorTrait, FailParser, Parser, ParseResults, ParserTrait, profile, RightData, RightDataSquasher, U8Set, VecY, vecx, Fail};
+use crate::SeqParser;
+
 #[macro_export]
 macro_rules! define_seq {
-    ($name:ident, $($child:ident),+) => {
+    ($seq_name:ident, $first:ident, $($rest:ident),+) => {
         #[derive(Debug)]
-        pub struct $name<$($child),+>
+        pub struct $seq_name<$first, $($rest),+>
         where
-            $($child: CombinatorTrait),+
+            $($rest: CombinatorTrait),+
         {
-            $(pub(crate) $child: Rc<$child>),+
+            pub(crate) $first: $first,
+            $(pub(crate) $rest: Rc<$rest>),+
         }
 
-        impl<$($child),+> CombinatorTrait for $name<$($child),+>
+        impl<$first, $($rest),+> CombinatorTrait for $seq_name<$first, $($rest),+>
         where
-            $($child: CombinatorTrait + 'static),+
+            $first: CombinatorTrait + 'static,
+            $($rest: CombinatorTrait + 'static),+
         {
             fn parse(&self, right_data: RightData, bytes: &[u8]) -> (Parser, ParseResults) {
                 let start_position = right_data.right_data_inner.fields1.position;
 
-                let mut parsers = Vec::new();
+                let first_combinator = &self.$first;
+                let (first_parser, first_parse_results) = profile!(stringify!($seq_name, " first child parse"), {
+                    first_combinator.parse(right_data, &bytes)
+                });
+
+                let done = first_parse_results.done();
+                if done && first_parse_results.right_data_vec.is_empty() {
+                    // Shortcut
+                    return (Parser::FailParser(FailParser), first_parse_results);
+                }
+
+                let mut parsers = if done {
+                    vec![]
+                } else {
+                    vec![(0, first_parser)]
+                };
+
                 let mut final_right_data = VecY::new();
-                let mut next_right_data_vec = vecx![right_data];
+                let mut next_right_data_vec = first_parse_results.right_data_vec;
+
+                fn helper<T: CombinatorTrait>(right_data: RightData, next_combinator: &Rc<T>, bytes: &[u8], start_position: usize, parsers: &mut Vec<(usize, Parser)>) -> VecY<RightData> {
+                    let offset = right_data.right_data_inner.fields1.position - start_position;
+                    let (parser, parse_results) = profile!(stringify!($seq_name, " child parse"), {
+                        next_combinator.parse(right_data, &bytes[offset..])
+                    });
+                    if !parse_results.done() {
+                        parsers.push((parsers.len(), parser));
+                    }
+                    parse_results.right_data_vec
+                }
 
                 $(
                     let mut next_next_right_data_vec = VecY::new();
                     for right_data in next_right_data_vec {
-                        let offset = right_data.right_data_inner.fields1.position - start_position;
-                        let (parser, parse_results) = profile!(concat!("seq ", stringify!($name), " child parse"), {
-                            self.$child.parse(right_data, &bytes[offset..])
-                        });
-                        if !parse_results.done() {
-                            parsers.push((parsers.len(), parser));
-                        }
-                        next_next_right_data_vec.extend(parse_results.right_data_vec);
+                        next_next_right_data_vec.extend(helper(right_data, &self.$rest, &bytes, start_position, &mut parsers));
                     }
                     next_right_data_vec = next_next_right_data_vec;
-
-                    if next_right_data_vec.is_empty() {
-                        return (Parser::FailParser(FailParser), ParseResults::new(VecY::new(), true));
-                    }
                 )+
 
                 final_right_data = next_right_data_vec;
@@ -47,7 +70,7 @@ macro_rules! define_seq {
 
                 let parser = Parser::SeqParser(SeqParser {
                     parsers,
-                    combinators: Rc::new(vecx![$(Combinator::DynRc(self.$child.clone())),+]),
+                    combinators: Rc::new(vecx![Combinator::Fail(Fail), $(Combinator::DynRc(self.$rest.clone())),+]),
                     position: start_position + bytes.len(),
                 });
 
@@ -56,19 +79,19 @@ macro_rules! define_seq {
                 (parser.into(), parse_results)
             }
         }
-
-        #[macro_export]
-        macro_rules! $name {
-            ($($child_expr:expr),+) => {
-                $crate::$name {
-                    $($child: Rc::new($child_expr)),+
-                }
-            };
-        }
     };
 }
 
-define_seq!(Seq2, A, B);
-define_seq!(Seq3, A, B, C);
-define_seq!(Seq4, A, B, C, D);
-// ... and so on
+define_seq!(Seq2, first, second);
+define_seq!(Seq3, first, second, third);
+// You can define more SeqN as needed
+
+#[macro_export]
+macro_rules! seq2 {
+    ($first:expr, $second:expr) => {
+        $crate::Seq2 {
+            first: Rc::new($first),
+            second: Rc::new($second),
+        }
+    };
+}
