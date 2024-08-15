2c371079b967f724341f6d695e437543619bf9ed
IsaacBreen <mail@isaacbreen.com>
2024-08-15 16:03:11 +0800

    try implement again

--- jul1/src/my_tinyvec.rs
+++ jul1/src/my_tinyvec.rs
@@ -1,6 +1,6 @@
 use std::hash::Hash;
 use std::iter::FromIterator;
-use std::ops::{Index, IndexMut, RangeBounds};
+use std::ops::{Index, IndexMut, RangeBounds, Bound};
 
 #[derive(Debug, Default, Clone, PartialEq, Eq, Hash)]
 pub(crate) enum FastVec<T> {
@@ -16,7 +16,26 @@ impl<T> FastVec<T> {
     }
 
     pub fn push(&mut self, item: T) {
-        todo!()
+        match self {
+            FastVec::None => {
+                // Transition from None to One
+                *self = FastVec::One(item);
+            }
+            FastVec::One(existing_item) => {
+                // Transition from One to Many
+                let mut vec = Vec::with_capacity(2);
+                unsafe {
+                    // Move the existing item into the vector
+                    vec.push(std::ptr::read(existing_item));
+                }
+                vec.push(item);
+                *self = FastVec::Many(vec);
+            }
+            FastVec::Many(vec) => {
+                // Simply push the item into the existing vector
+                vec.push(item);
+            }
+        }
     }
 
     pub fn len(&self) -> usize {
@@ -94,15 +113,90 @@ impl<T> FastVec<T> {
     }
 
     pub fn append(&mut self, other: &mut Self) {
-        todo!()
+        match (&mut *self, &mut *other) {
+            (FastVec::None, _) => {
+                // Move the contents of other into self
+                std::mem::swap(self, other);
+            }
+            (_, FastVec::None) => {
+                // Do nothing, as other is empty
+            }
+            (FastVec::One(self_item), FastVec::One(other_item)) => {
+                // Move self's item to a new vector, then append other's item
+                let mut vec = Vec::with_capacity(2);
+                unsafe {
+                    vec.push(std::ptr::read(self_item));
+                    vec.push(std::ptr::read(other_item));
+                }
+                *self = FastVec::Many(vec);
+                *other = FastVec::None;
+            }
+            (FastVec::One(self_item), FastVec::Many(other_vec)) => {
+                // Move self's item to the front of other's vector, then take ownership
+                let mut vec = std::mem::take(other_vec);
+                unsafe {
+                    vec.insert(0, std::ptr::read(self_item));
+                }
+                *self = FastVec::Many(vec);
+                *other = FastVec::None;
+            }
+            (FastVec::Many(self_vec), FastVec::One(other_item)) => {
+                // Append other's item to self's vector
+                unsafe {
+                    self_vec.push(std::ptr::read(other_item));
+                }
+                *other = FastVec::None;
+            }
+            (FastVec::Many(self_vec), FastVec::Many(other_vec)) => {
+                // Append other's vector to self's vector
+                self_vec.append(other_vec);
+            }
+        }
     }
 
     pub fn iter(&self) -> impl Iterator<Item = &T> {
-        todo!()
+        match self {
+            FastVec::None => [].iter(),
+            FastVec::One(item) => std::slice::from_ref(item).iter(),
+            FastVec::Many(vec) => vec.iter(),
+        }
     }
 
     pub fn drain(&mut self, range: impl RangeBounds<usize>) -> impl Iterator<Item = T> {
-        todo!()
+        let start = match range.start_bound() {
+            Bound::Included(&start) => start,
+            Bound::Excluded(&start) => start + 1,
+            Bound::Unbounded => 0,
+        };
+        let end = match range.end_bound() {
+            Bound::Included(&end) => end + 1,
+            Bound::Excluded(&end) => end,
+            Bound::Unbounded => self.len(),
+        };
+
+        match self {
+            FastVec::None => Vec::new().into_iter(),
+            FastVec::One(_) => {
+                if start == 0 && end > 0 {
+                    if let FastVec::One(item) = std::mem::replace(self, FastVec::None) {
+                        vec![item].into_iter()
+                    } else {
+                        unreachable!()
+                    }
+                } else {
+                    Vec::new().into_iter()
+                }
+            }
+            FastVec::Many(vec) => {
+                let drained: Vec<_> = vec.drain(start..end).collect();
+                if vec.len() == 1 {
+                    if let Some(last_item) = vec.pop() {
+                        *self = FastVec::One(last_item);
+                    }
+                }
+                drained.into_iter()
+            }
+        }
     }
 }
 
@@ -141,13 +235,33 @@ impl<T> Index<usize> for FastVec<T> {
     type Output = T;
 
     fn index(&self, index: usize) -> &Self::Output {
-        todo!()
+        match self {
+            FastVec::None => panic!("Index out of bounds"),
+            FastVec::One(item) => {
+                if index == 0 {
+                    item
+                } else {
+                    panic!("Index out of bounds")
+                }
+            }
+            FastVec::Many(vec) => &vec[index],
+        }
     }
 }
 
 impl<T> IndexMut<usize> for FastVec<T> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        todo!()
+        match self {
+            FastVec::None => panic!("Index out of bounds"),
+            FastVec::One(item) => {
+                if index == 0 {
+                    item
+                } else {
+                    panic!("Index out of bounds")
+                }
+            }
+            FastVec::Many(vec) => &mut vec[index],
+        }
     }
 }
 
@@ -183,7 +297,7 @@ fn main() {
     assert_eq!(fast_vec.len(), 5);
     assert!(other_vec.is_empty());
 
-    let drained: Vec<_> = fast_vec.drain().collect();
+    let drained: Vec<_> = fast_vec.drain(..).collect();
     assert_eq!(drained, vec![3, 4, 5, 6, 7]);
     assert!(fast_vec.is_empty());
 }
