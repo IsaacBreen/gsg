c31d7e09144dd99d67b9efc87902b98d276c57b6
IsaacBreen <mail@isaacbreen.com>
2024-08-15 19:41:11 +0800

    trying to get compile working.

--- jul1/src/compiler.rs
+++ jul1/src/compiler.rs
@@ -20,39 +20,35 @@ impl From<Ref> for DeferredInner {
 impl Combinator {
     pub fn compile(mut self) -> Combinator {
         let mut deferred_cache: HashMap<DeferredFn, Ref> = HashMap::new();
-        fn compile_inner(combinator: &Combinator, deferred_cache: &mut HashMap<DeferredFn, Ref>) {
-            match combinator {
-                // Construct the deferred combinator
-                Combinator::Deferred(inner) => {
-                    let new_inner: DeferredInner = match inner.inner.borrow().deref() {
-                        DeferredInner::Uncompiled(f) => {
-                            if let Some(cached) = deferred_cache.get(f) {
-                                cached.clone().into()
-                            } else {
-                                let strong = strong_ref();
-                                let weak = strong.downgrade();
-                                deferred_cache.insert(*f, Ref::Weak(weak.clone()));
-                                let mut evaluated: Combinator = (f.0)();
-                                compile_inner(&mut evaluated, deferred_cache);
-                                strong.set(evaluated);
-                                deferred_cache.insert(*f, Ref::Strong(strong.clone()));
-                                DeferredInner::CompiledStrong(strong.clone())
-                            }
-                        }
-                        DeferredInner::CompiledStrong(strong) => {
+        fn compile_inner(combinator: &dyn CombinatorTrait, deferred_cache: &mut HashMap<DeferredFn, Ref>) {
+            if let Ok(Deferred { inner }) = todo!() {
+                let new_inner: DeferredInner = match inner.borrow().deref() {
+                    DeferredInner::Uncompiled(f) => {
+                        if let Some(cached) = deferred_cache.get(f) {
+                            cached.clone().into()
+                        } else {
+                            let strong = strong_ref();
+                            let weak = strong.downgrade();
+                            deferred_cache.insert(*f, Ref::Weak(weak.clone()));
+                            let mut evaluated: Combinator = (f.0)();
+                            compile_inner(&mut evaluated, deferred_cache);
+                            strong.set(evaluated);
+                            deferred_cache.insert(*f, Ref::Strong(strong.clone()));
                             DeferredInner::CompiledStrong(strong.clone())
                         }
-                        DeferredInner::CompiledWeak(weak) => {
-                            DeferredInner::CompiledWeak(weak.clone())
-                        }
-                    };
-                    *inner.inner.borrow_mut() = new_inner;
-                }
-                _ => {
-                    // combinator.apply(&mut |combinator| {
-                    //     compile_inner(combinator, deferred_cache);
-                    // });
-                }
+                    }
+                    DeferredInner::CompiledStrong(strong) => {
+                        DeferredInner::CompiledStrong(strong.clone())
+                    }
+                    DeferredInner::CompiledWeak(weak) => {
+                        DeferredInner::CompiledWeak(weak.clone())
+                    }
+                };
+                *inner.borrow_mut() = new_inner;
+            } else {
+                combinator.apply(&mut |combinator| {
+                    compile_inner(combinator, deferred_cache);
+                });
             }
         }
         compile_inner(&mut self, &mut deferred_cache);
