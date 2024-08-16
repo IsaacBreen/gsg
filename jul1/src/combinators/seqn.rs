9864be6c825d924779df84819b0f8b1c70f03538
IsaacBreen <mail@isaacbreen.com>
2024-08-16 15:40:03 +0800

    add one more seq level

--- jul1/src/combinators/seqn.rs
+++ jul1/src/combinators/seqn.rs
@@ -111,6 +111,7 @@ define_seq!(Seq6, c0, c1, c2, c3, c4, c5);
 define_seq!(Seq7, c0, c1, c2, c3, c4, c5, c6);
 define_seq!(Seq8, c0, c1, c2, c3, c4, c5, c6, c7);
 define_seq!(Seq9, c0, c1, c2, c3, c4, c5, c6, c7, c8);
+define_seq!(Seq10, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9);
 
 #[macro_export]
 macro_rules! seq {
@@ -193,10 +194,24 @@ macro_rules! seq {
             c8: std::rc::Rc::new($crate::IntoCombinator::into_combinator($c8)),
         }
     };
-    ($c0:expr, $c1:expr, $c2:expr, $c3:expr, $c4:expr, $c5:expr, $c6:expr, $c7:expr, $c8:expr, $($rest:expr),+ $(,)?) => {{
+    ($c0:expr, $c1:expr, $c2:expr, $c3:expr, $c4:expr, $c5:expr, $c6:expr, $c7:expr, $c8:expr, $c9:expr $(,)?) => {
+        $crate::Seq10 {
+            c0: $crate::IntoCombinator::into_combinator($c0),
+            c1: std::rc::Rc::new($crate::IntoCombinator::into_combinator($c1)),
+            c2: std::rc::Rc::new($crate::IntoCombinator::into_combinator($c2)),
+            c3: std::rc::Rc::new($crate::IntoCombinator::into_combinator($c3)),
+            c4: std::rc::Rc::new($crate::IntoCombinator::into_combinator($c4)),
+            c5: std::rc::Rc::new($crate::IntoCombinator::into_combinator($c5)),
+            c6: std::rc::Rc::new($crate::IntoCombinator::into_combinator($c6)),
+            c7: std::rc::Rc::new($crate::IntoCombinator::into_combinator($c7)),
+            c8: std::rc::Rc::new($crate::IntoCombinator::into_combinator($c8)),
+            c9: std::rc::Rc::new($crate::IntoCombinator::into_combinator($c9)),
+        }
+    };
+    ($c0:expr, $c1:expr, $c2:expr, $c3:expr, $c4:expr, $c5:expr, $c6:expr, $c7:expr, $c8:expr, $c9:expr, $($rest:expr),+ $(,)?) => {{
         fn convert(x: impl $crate::IntoCombinator + 'static) -> Box<dyn $crate::CombinatorTrait> {
             Box::new(x.into_combinator())
         }
-        $crate::_seq(vec![convert($c0), convert($c1), convert($c2), convert($c3), convert($c4), convert($c5), convert($c6), convert($c7), convert($c8), $(convert($rest)),+])
+        $crate::_seq(vec![convert($c0), convert($c1), convert($c2), convert($c3), convert($c4), convert($c5), convert($c6), convert($c7), convert($c8), convert($c9), $(convert($rest)),+])
     }};
 }
