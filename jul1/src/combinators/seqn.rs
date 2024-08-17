3a70684da80e9e9cc4b2c79832f4f97840d3b994
IsaacBreen <mail@isaacbreen.com>
2024-08-17 16:07:20 +0800

    Revert "implement parser for each N"
    This reverts commit 526e0da3cb7fdc8a1e9ceedafe0a06b63aadf58a.

--- jul1/src/combinators/seqn.rs
+++ jul1/src/combinators/seqn.rs
@@ -1,6 +1,7 @@
 use std::rc::Rc;
 use std::collections::BTreeMap;
 use crate::{Combinator, CombinatorTrait, FailParser, Parser, ParseResults, ParserTrait, profile, RightData, RightDataSquasher, U8Set, VecY, vecx, Fail, IntoCombinator};
+use crate::SeqParser;
 
 macro_rules! profile {
     ($name:expr, $body:expr) => {
@@ -10,7 +11,7 @@ macro_rules! profile {
 
 #[macro_export]
 macro_rules! define_seq {
-    ($seq_name:ident, $seq_parser_name:ident, $first:ident, $($rest:ident),+ $(,)?) => {
+    ($seq_name:ident, $first:ident, $($rest:ident),+ $(,)?) => {
         #[derive(Debug)]
         pub struct $seq_name<$first, $($rest),+>
         where
@@ -18,16 +19,6 @@ macro_rules! define_seq {
         {
             pub(crate) $first: $first,
             $(pub(crate) $rest: $rest),+
-        }
-
-        #[derive(Debug)]
-        pub struct $seq_parser_name<'a, $first, $($rest),+>
-        where
-            $($rest: CombinatorTrait),+
-        {
-            pub(crate) combinator: &'a $seq_name<$first, $($rest),+>,
-            pub(crate) parsers: Vec<(usize, Parser<'a>)>,
-            pub(crate) position: usize,
         }
 
         impl<$first, $($rest),+> CombinatorTrait for $seq_name<$first, $($rest),+>
@@ -83,11 +74,12 @@ macro_rules! define_seq {
                         return (Parser::FailParser(FailParser), ParseResults::new(final_right_data, true));
                     }
 
-                    let parser = Parser::DynParser(Box::new($seq_parser_name {
-                        combinator: self,
+                    let parser = Parser::SeqParser(SeqParser {
                         parsers,
+                        // todo: actually, this is invalid and causes an error. The children don't necessarily implement `Clone`.
+                        combinators: vecx![$crate::IntoDyn::into_dyn(Fail), $($crate::IntoDyn::into_dyn(self.$rest.clone())),+],
                         position: start_position + bytes.len(),
-                    }));
+                    });
 
                     let parse_results = ParseResults::new(final_right_data, false);
 
@@ -109,96 +101,24 @@ macro_rules! define_seq {
                 finalizer(next_right_data_vec, parsers)
             }
         }
-
-        impl<'a, $first, $($rest),+> ParserTrait for $seq_parser_name<'a, $first, $($rest),+>
-        where
-            $first: CombinatorTrait,
-            $($rest: CombinatorTrait),+
-        {
-            fn get_u8set(&self) -> U8Set {
-                let mut u8set = U8Set::none();
-                for (_, parser) in &self.parsers {
-                    u8set = u8set.union(&parser.get_u8set());
-                }
-                u8set
-            }
-
-            fn parse(&mut self, bytes: &[u8]) -> ParseResults {
-                profile!(stringify!($seq_parser_name, "::parse"), {
-                    let mut final_right_data: VecY<RightData> = VecY::new();
-                    let mut parser_initialization_queue: BTreeMap<usize, RightDataSquasher> = BTreeMap::new();
-
-                    self.parsers.retain_mut(|(combinator_index, parser)| {
-                        let parse_results = profile!(stringify!($seq_parser_name, "::parse child Parser::parse"), {
-                            parser.parse(bytes)
-                        });
-                        let done = parse_results.done();
-                        if *combinator_index + 1 < self.combinator.children().len() {
-                            profile!(stringify!($seq_parser_name, "::parse extend parser_initialization_queue"), {
-                                parser_initialization_queue.entry(*combinator_index + 1).or_default().extend(parse_results.right_data_vec);
-                            });
-                        } else {
-                            profile!(stringify!($seq_parser_name, "::parse extend final_right_data"), {
-                                final_right_data.extend(parse_results.right_data_vec);
-                            });
-                        }
-                        !done
-                    });
-
-                    while let Some((combinator_index, right_data_squasher)) = parser_initialization_queue.pop_first() {
-                        for right_data in right_data_squasher.finish() {
-                            let offset = right_data.right_data_inner.fields1.position - self.position;
-                            let combinator = self.combinator.children().get(combinator_index).unwrap();
-                            let (parser, parse_results) = profile!(stringify!($seq_parser_name, "::parse child Combinator::parse"), {
-                                combinator.parse(right_data, &bytes[offset..])
-                            });
-                            if !parse_results.done() {
-                                self.parsers.push((combinator_index, parser));
-                            }
-                            if combinator_index + 1 < self.combinator.children().len() {
-                                profile!(stringify!($seq_parser_name, "::parse extend parser_initialization_queue"), {
-                                    parser_initialization_queue.entry(combinator_index + 1).or_default().extend(parse_results.right_data_vec);
-                                });
-                            } else {
-                                final_right_data.extend(parse_results.right_data_vec);
-                            }
-                        }
-                    }
-
-                    self.position += bytes.len();
-
-                    ParseResults::new(final_right_data, self.parsers.is_empty())
-                })
-            }
-        }
-
-        impl<$first, $($rest),+> $seq_name<$first, $($rest),+>
-        where
-            $first: CombinatorTrait,
-            $($rest: CombinatorTrait),+
-        {
-            fn children(&self) -> Vec<&dyn CombinatorTrait> {
-                vec![&self.$first, $( &self.$rest ),+]
-            }
-        }
     };
 }
 
-define_seq!(Seq2, Seq2Parser, c0, c1);
-define_seq!(Seq3, Seq3Parser, c0, c1, c2);
-define_seq!(Seq4, Seq4Parser, c0, c1, c2, c3);
-define_seq!(Seq5, Seq5Parser, c0, c1, c2, c3, c4);
-define_seq!(Seq6, Seq6Parser, c0, c1, c2, c3, c4, c5);
-define_seq!(Seq7, Seq7Parser, c0, c1, c2, c3, c4, c5, c6);
-define_seq!(Seq8, Seq8Parser, c0, c1, c2, c3, c4, c5, c6, c7);
-define_seq!(Seq9, Seq9Parser, c0, c1, c2, c3, c4, c5, c6, c7, c8);
-define_seq!(Seq10, Seq10Parser, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9);
-define_seq!(Seq11, Seq11Parser, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10);
-define_seq!(Seq12, Seq12Parser, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11);
-define_seq!(Seq13, Seq13Parser, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12);
-define_seq!(Seq14, Seq14Parser, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13);
-define_seq!(Seq15, Seq15Parser, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14);
-define_seq!(Seq16, Seq16Parser, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15);
+define_seq!(Seq2, c0, c1);
+define_seq!(Seq3, c0, c1, c2);
+define_seq!(Seq4, c0, c1, c2, c3);
+define_seq!(Seq5, c0, c1, c2, c3, c4);
+define_seq!(Seq6, c0, c1, c2, c3, c4, c5);
+define_seq!(Seq7, c0, c1, c2, c3, c4, c5, c6);
+define_seq!(Seq8, c0, c1, c2, c3, c4, c5, c6, c7);
+define_seq!(Seq9, c0, c1, c2, c3, c4, c5, c6, c7, c8);
+define_seq!(Seq10, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9);
+define_seq!(Seq11, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10);
+define_seq!(Seq12, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11);
+define_seq!(Seq13, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12);
+define_seq!(Seq14, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13);
+define_seq!(Seq15, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14);
+define_seq!(Seq16, c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15);
 
 pub fn seqn_helper<T: IntoCombinator>(x: T) -> T::Output {
     IntoCombinator::into_combinator(x)
