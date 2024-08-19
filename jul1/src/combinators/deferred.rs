6c8500746f67dca9aeab2dbeebc5c3c5851eaefa
IsaacBreen <mail@isaacbreen.com>
2024-08-19 12:02:48 +0800

    Revert "nicer"
    This reverts commit d598264405c44333b2d32537729ece74ec9e64e9.

--- jul1/src/combinators/deferred.rs
+++ jul1/src/combinators/deferred.rs
@@ -51,16 +51,14 @@ pub trait DeferredFnTrait<T: CombinatorTrait + Clone + 'static>: Debug {
 struct CacheEntry {
     value: Box<dyn Any>,
     type_name: String,
-    debug_value: Box<dyn Debug>,
+    value_str: String,
 }
 
-impl Debug for CacheEntry {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("CacheEntry")
-            .field("type_name", &self.type_name)
-            .field("debug_value", &self.debug_value)
-            .finish_non_exhaustive()
-    }
+// Function for printing cache entries
+fn print_entry(entry: &CacheEntry) {
+    let CacheEntry { value: _, type_name, value_str } = entry;
+    eprintln!("  - type_name: {}", type_name);
+    eprintln!("  - value_str: {}", value_str);
 }
 
 impl<T: CombinatorTrait + Clone + 'static, F: Fn() -> T> DeferredFnTrait<T> for DeferredFn<T, F> {
@@ -75,26 +73,28 @@ impl<T: CombinatorTrait + Clone + 'static, F: Fn() -> T> DeferredFnTrait<T> for 
                     eprintln!("Cache dump:");
                     for (addr, entry) in cache.iter() {
                         eprintln!("- cache entry, addr: {}", addr);
-                        eprintln!("{:?}", entry);
+                        print_entry(entry);
                     }
                     let actual_type_name = std::any::type_name::<T>();
+                    let actual_value = self.0();
+                    let actual_value_str = format!("{:#?}", actual_value);
                     let actual_entry = CacheEntry {
-                        value: Box::new(self.0()),
+                        value: Box::new(actual_value),
                         type_name: actual_type_name.to_string(),
-                        debug_value: Box::new(self.0()),
+                        value_str: actual_value_str,
                     };
-                    eprintln!("matched cache entry, addr: {}", self.1);
-                    eprintln!("{:#?}", entry);
-                    eprintln!("actual value, addr: {}", self.1);
-                    eprintln!("{:#?}", actual_entry);
+                    eprintln!("- matched cache entry, addr: {}", self.1);
+                    print_entry(entry);
+                    eprintln!("- actual value, addr: {}", self.1);
+                    print_entry(&actual_entry);
 
                     panic!("Expected value at address {} to be of typeid {:?}, but it had typeid {:?}", self.1, std::any::TypeId::of::<T>(), entry.value.type_id());
                 }
             } else {
                 let value = (self.0)();
                 let type_name = std::any::type_name::<T>().to_string();
-                let debug_value = Box::new(value.clone());
-                cache.insert(self.1, CacheEntry { value: Box::new(value.clone()), type_name, debug_value });
+                let value_str = format!("{:#?}", value);
+                cache.insert(self.1, CacheEntry { value: Box::new(value.clone()), type_name, value_str });
                 value
             }
         })
