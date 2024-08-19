a53f36bc721a106ec91cd6be55531b584040a609
IsaacBreen <mail@isaacbreen.com>
2024-08-19 13:48:12 +0800

    add todo

--- jul1/src/combinators/cache.rs
+++ jul1/src/combinators/cache.rs
@@ -229,6 +229,7 @@ pub fn cache_context<'a, T: IntoCombinator>(a: T)-> impl CombinatorTrait {
     profile_internal("cache_context", CacheContext { inner: a.into_combinator() })
 }
 
+// todo: do we really need to make this 'static?
 pub fn cached<T: IntoCombinator>(a: T)-> impl CombinatorTrait where T::Output: 'static {
     profile_internal("cached", Cached { inner: Rc::new(a.into_combinator()) })
 }
