3666c8962d42bd030bb38226fb34358583e4e349
IsaacBreen <mail@isaacbreen.com>
2024-08-19 17:19:48 +0800

    add unambiguous_parse

--- jul1/src/parse_state/mod.rs
+++ jul1/src/parse_state/mod.rs
@@ -2,7 +2,9 @@ pub use parse_results::*;
 pub use right_data::*;
 pub use traits::*;
 pub use crate::combinators::*;
+pub use unambiguous_parse_results::*;
 
 mod parse_results;
 mod right_data;
 mod traits;
+mod unambiguous_parse_results;
---
+++ jul1/src/parse_state/unambiguous_parse_results.rs
@@ -0,0 +1,44 @@
+use crate::{RightData, Squash, vecy};
+use crate::internal_vec::VecY;
+use crate::VecX;
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct UnambiguousParseResults {
+    pub option_right_data: Option<RightData>,
+    pub done: bool,
+}
+
+impl UnambiguousParseResults {
+    pub fn done(&self) -> bool {
+        self.done
+    }
+    pub fn new(right_data: RightData, done: bool) -> Self {
+        UnambiguousParseResults {
+            option_right_data: Some(right_data),
+            done,
+        }
+    }
+    pub fn new_single(right_data: RightData, done: bool) -> Self {
+        UnambiguousParseResults {
+            option_right_data: Some(right_data),
+            done,
+        }
+    }
+    pub fn empty(done: bool) -> Self {
+        UnambiguousParseResults {
+            option_right_data: None,
+            done,
+        }
+    }
+    pub fn empty_unfinished() -> Self {
+        UnambiguousParseResults::empty(false)
+    }
+    pub fn empty_finished() -> Self {
+        UnambiguousParseResults::empty(true)
+    }
+    pub fn succeeds_decisively(&self) -> bool {
+        self.done() && !self.option_right_data.is_none() && !self.option_right_data.as_ref().unwrap().failable()
+        // TODO: remove the below line and uncomment the above line
+        // self.done() && !self.option_right_data.is_empty()
+    }
+}
