a57cdd898ff48053393a640517631d572a2e20f3
IsaacBreen <mail@isaacbreen.com>
2024-08-20 14:44:27 +0800

    asdf

--- jul1/src/python/test_python.rs
+++ jul1/src/python/test_python.rs
@@ -260,9 +260,10 @@ fn test_actual_python_file_fast() {
     let test_cases = [
         // ("Simple string", "x = 12\nx = 2\nx"),
         // ("dump_python_gram.py", include_str!("../python/dump_python_gram.py")),
-        ("remove_left_recursion.py", include_str!("../python/remove_left_recursion.py")),
+        // ("remove_left_recursion.py", include_str!("../python/remove_left_recursion.py")),
         // ("test_input.py", include_str!("../tests/test_input.py")),
         // ("test_input2.py", include_str!("../tests/test_input2.py")),
+        ("test_input2.py", include_str!("../tests/test_input3.py")),
         // ("test_input_simplified.py", include_str!("../tests/test_input_simplified.py")),
     ];
 
