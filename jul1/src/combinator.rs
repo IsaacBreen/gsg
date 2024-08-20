9571364425c64241a8dbdf1b141ebb55a595f7b5
IsaacBreen <mail@isaacbreen.com>
2024-08-20 15:43:56 +0800

    ah

--- jul1/src/combinator.rs
+++ jul1/src/combinator.rs
@@ -130,7 +130,7 @@ pub trait CombinatorTrait: std::fmt::Debug {
     fn apply(&self, f: &mut dyn FnMut(&dyn CombinatorTrait)) {}
     fn old_parse(&self, right_data: RightData, bytes: &[u8]) -> (Parser, ParseResults);
     fn parse(&self, right_data: RightData, bytes: &[u8]) -> (Parser, ParseResults) {
-        // return self.parse(right_data, bytes);
+        // return self.old_parse(right_data, bytes);
         let parse_result = self.one_shot_parse(right_data.clone(), bytes);
         match parse_result {
             Ok(right_data) => (Parser::FailParser(FailParser), ParseResults::new_single(right_data, true)),
