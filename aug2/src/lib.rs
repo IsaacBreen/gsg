f78b50cb4fee2680276a3ff11a624e4f43c53857
IsaacBreen <mail@isaacbreen.com>
2024-08-22 11:41:51 +0800

    hm

--- aug2/src/lib.rs
+++ aug2/src/lib.rs
@@ -10,40 +10,21 @@ pub trait CombinatorTrait: BaseCombinatorTrait {
     type Parser: ParserTrait;
     fn parse(&self, right_data: RightData, bytes: &[u8]) -> (Self::Parser, ParseResults);
 }
-
 pub trait BaseCombinatorTrait {
     fn as_any(&self) -> &dyn std::any::Any;
     fn apply_to_children(&self, f: &mut dyn FnMut(&dyn BaseCombinatorTrait)) {}
 }
-
 pub trait ParserTrait {
     fn parse(&mut self, bytes: &[u8]) -> ParseResults;
-}
-
-impl<T: CombinatorTrait> CombinatorTrait for Box<T> {
-    type Parser = T::Parser;
-
-    fn parse(&self, right_data: RightData, bytes: &[u8]) -> (Self::Parser, ParseResults) {
-        (**self).parse(right_data, bytes)
-    }
-}
-
-impl<T: CombinatorTrait> BaseCombinatorTrait for Box<T> {
-    fn as_any(&self) -> &dyn std::any::Any {
-        (**self).as_any()
-    }
-    fn apply_to_children(&self, f: &mut dyn FnMut(&dyn BaseCombinatorTrait)) {
-        (**self).apply_to_children(f);
-    }
 }
 
 struct Wrapper<T> {
     inner: T,
 }
-struct WrapperParser<T> {
-    inner: T,
+struct WrapperParser<'a, T: CombinatorTrait> {
+    combinator: &'a T,
+    inner: T::Parser,
 }
-
 impl<T: BaseCombinatorTrait> BaseCombinatorTrait for Wrapper<T> {
     fn as_any(&self) -> &dyn std::any::Any {
         self.inner.as_any()
@@ -52,11 +33,38 @@ impl<T: BaseCombinatorTrait> BaseCombinatorTrait for Wrapper<T> {
         self.inner.apply_to_children(f);
     }
 }
+impl<'a, T: CombinatorTrait> CombinatorTrait for Wrapper<T> {
+    type Parser = WrapperParser<'a, T>;
+
+    fn parse(&self, right_data: RightData, bytes: &[u8]) -> (Self::Parser, ParseResults) {
+        let (inner, results) = self.inner.parse(right_data, bytes);
+        (WrapperParser { combinator: self, inner }, results)
+    }
+}
 impl<T: ParserTrait> ParserTrait for Wrapper<T> {
     fn parse(&mut self, bytes: &[u8]) -> ParseResults {
         self.inner.parse(bytes)
     }
 }
+
+// struct DynWrapper<T> {
+//     inner: T,
+// }
+// impl<T: BaseCombinatorTrait> BaseCombinatorTrait for DynWrapper<T> {
+//     fn as_any(&self) -> &dyn std::any::Any {
+//         self.inner.as_any()
+//     }
+//     fn apply_to_children(&self, f: &mut dyn FnMut(&dyn BaseCombinatorTrait)) {
+//         self.inner.apply_to_children(f);
+//     }
+// }
+// impl<T: CombinatorTrait> CombinatorTrait for DynWrapper<T> {
+//     type Parser = Box<dyn ParserTrait>;
+//
+//     fn parse(&self, right_data: RightData, bytes: &[u8]) -> (Self::Parser, ParseResults) {
+//         self.inner.parse(right_data, bytes)
+//     }
+// }
 
 #[test]
 fn test() {}
