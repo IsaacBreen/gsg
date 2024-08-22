77e40bf346d9023acfe626e50ef2e77d1ddcf4bb
IsaacBreen <mail@isaacbreen.com>
2024-08-22 11:34:35 +0800

    wrapper

--- aug2/src/lib.rs
+++ aug2/src/lib.rs
@@ -6,7 +6,7 @@ type ParseResults = ();
 type UnambiguousParseResults = ();
 type U8Set = ();
 
-pub trait CombinatorTrait: BaseCombinatorTrait + std::fmt::Debug {
+pub trait CombinatorTrait: BaseCombinatorTrait {
     type Parser: ParserTrait;
     fn parse(&self, right_data: RightData, bytes: &[u8]) -> (Self::Parser, ParseResults);
 }
@@ -16,11 +16,11 @@ pub trait BaseCombinatorTrait {
     fn apply_to_children(&self, f: &mut dyn FnMut(&dyn BaseCombinatorTrait)) {}
 }
 
-pub trait ParserTrait: std::fmt::Debug {
+pub trait ParserTrait {
     fn parse(&mut self, bytes: &[u8]) -> ParseResults;
 }
 
-impl<T: CombinatorTrait + ?Sized> CombinatorTrait for Box<T> {
+impl<T: CombinatorTrait> CombinatorTrait for Box<T> {
     type Parser = T::Parser;
 
     fn parse(&self, right_data: RightData, bytes: &[u8]) -> (Self::Parser, ParseResults) {
@@ -28,7 +28,7 @@ impl<T: CombinatorTrait + ?Sized> CombinatorTrait for Box<T> {
     }
 }
 
-impl<T: CombinatorTrait + ?Sized> BaseCombinatorTrait for Box<T> {
+impl<T: CombinatorTrait> BaseCombinatorTrait for Box<T> {
     fn as_any(&self) -> &dyn std::any::Any {
         (**self).as_any()
     }
@@ -36,3 +36,27 @@ impl<T: CombinatorTrait + ?Sized> BaseCombinatorTrait for Box<T> {
         (**self).apply_to_children(f);
     }
 }
+
+struct Wrapper<T> {
+    inner: T,
+}
+struct WrapperParser<T> {
+    inner: T,
+}
+
+impl<T: BaseCombinatorTrait> BaseCombinatorTrait for Wrapper<T> {
+    fn as_any(&self) -> &dyn std::any::Any {
+        self.inner.as_any()
+    }
+    fn apply_to_children(&self, f: &mut dyn FnMut(&dyn BaseCombinatorTrait)) {
+        self.inner.apply_to_children(f);
+    }
+}
+impl<T: ParserTrait> ParserTrait for Wrapper<T> {
+    fn parse(&mut self, bytes: &[u8]) -> ParseResults {
+        self.inner.parse(bytes)
+    }
+}
+
+#[test]
+fn test() {}
