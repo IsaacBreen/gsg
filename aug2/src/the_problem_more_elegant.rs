fa10d3bc4654a82b977e50fc7207378738cb9071
IsaacBreen <mail@isaacbreen.com>
2024-08-23 12:29:27 +0800

    frick

--- aug2/src/the_problem_more_elegant.rs
+++ aug2/src/the_problem_more_elegant.rs
@@ -1,26 +1,38 @@
 use std::fmt::Display;
 
+enum ParseResult {
+    Ok,    // this means 'parse is ok so far, but expects to eat more bytes'
+    Done,
+    Err,
+}
+
 pub trait CombinatorTrait {
     type Parser<'a>: ParserTrait where Self: 'a;
     fn init_parser<'a>(&'a self) -> Self::Parser<'a>;
 }
 pub trait ParserTrait {
-    fn parse(&mut self, bytes: &[u8]) -> bool;
+    fn parse(&mut self, byte: u8) -> ParseResult;
 }
 
 struct Eat {
     byte: u8,
 }
-struct EatParser;
+struct EatParser {
+    byte: u8,
+}
 impl CombinatorTrait for Eat {
     type Parser<'a> = EatParser;
     fn init_parser<'a>(&'a self) -> Self::Parser<'a> {
-        EatParser
+        EatParser { byte: self.byte }
     }
 }
 impl ParserTrait for EatParser {
-    fn parse(&mut self, bytes: &[u8]) -> bool {
-        todo!()
+    fn parse(&mut self, byte: u8) -> ParseResult {
+        if byte == self.byte {
+            ParseResult::Done
+        } else {
+            ParseResult::Err
+
     }
 }
 
@@ -30,25 +42,27 @@ struct Seq<L: CombinatorTrait, R: CombinatorTrait> {
 }
 enum SeqParser<'a, L: CombinatorTrait, R: CombinatorTrait> where Self: 'a {
     Left {
-        // Left parser is active, so store it
         left: L::Parser<'a>,
-        // Store the right parser so we can initialize it once the left parser is done
         right: &'a R,
     },
     Right {
         right: R::Parser<'a>,
-        // No need to store the left parser here - we're not going to backtrack to it
     },
-    // Why not initialise both parsers at the same time? One reason is performance.
-    // We'd have to initialise the whole grammar at once, even though there are many parts of it
-    // that we might never touch during a particular parse.
-    // A more important reason is that the parser would fail if we had any recursion in our grammar.
-    // But this example is already too complex, so I'm not going to get into defining recursive
-    // combinators.
 }
 impl<L: CombinatorTrait, R: CombinatorTrait> ParserTrait for SeqParser<'_, L, R> {
-    fn parse(&mut self, bytes: &[u8]) -> bool {
-        todo!()
+    fn parse(&mut self, byte: u8) -> ParseResult {
+        match self {
+            SeqParser::Left { left, right } => {
+                let result = left.parse(byte);
+                *self = SeqParser::Right {
+                    right: right.init_parser(),
+                };
+                result
+            }
+            SeqParser::Right { right } => {
+                right.parse(byte)
+            }
+        }
     }
 }
 impl<L: CombinatorTrait, R: CombinatorTrait> CombinatorTrait for Seq<L, R> {
@@ -68,15 +82,15 @@ pub struct DynParser<'a> {
     inner: Box<dyn ParserTrait + 'a>,
 }
 impl<T: CombinatorTrait> CombinatorTrait for DynCombinator<T> {
-    type Parser<'a> = DynParser<'a> where Self: 'a;
+    type Parser<'a> = Box<dyn ParserTrait + 'a> where Self: 'a;
     fn init_parser<'a>(&'a self) -> Self::Parser<'a> {
         let inner = self.inner.init_parser();
-        DynParser { inner: Box::new(inner) }
+        Box::new(inner)
     }
 }
-impl ParserTrait for DynParser<'_> {
-    fn parse(&mut self, bytes: &[u8]) -> bool {
-        self.inner.parse(bytes)
+impl ParserTrait for Box<dyn ParserTrait + '_> {
+    fn parse(&mut self, byte: u8) -> ParseResult {
+        (**self).parse(byte)
     }
 }
 
@@ -97,7 +111,12 @@ fn test() {
     let eat_b = Eat { byte: b'b' };
     let eat_ab = seq(eat_a, eat_b);
     let dyn_eat_ab = make_dyn(eat_ab);
+
     let mut parser = dyn_eat_ab.init_parser();
-    assert!(parser.parse(&b"ab"[..]));
-    assert!(!parser.parse(&b"ac"[..]));
+    assert!(parser.parse(b'a'));
+    assert!(parser.parse(b'b'));
+
+    let mut parser = dyn_eat_ab.init_parser();
+    assert!(parser.parse(b'a'));
+    assert!(!parser.parse(b'c'));
 }
