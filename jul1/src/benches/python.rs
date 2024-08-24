cb5fcbdf824eb663293239629c771411991ddc51
IsaacBreen <mail@isaacbreen.com>
2024-08-24 15:08:10 +0800

    f

--- jul1/src/benches/python.rs
+++ jul1/src/benches/python.rs
@@ -1,12 +1,12 @@
-use std::path::Path;
-use std::time::Instant;
-use jun3::python_file;
-use jun3::utils::{assert_parses, assert_parses_fast};
-
-fn main() {
-    println!("Running Python benchmarks...");
-    let path = Path::new("src/tests/test_input.py");
-    let file = std::fs::read_to_string(path).unwrap();
-    let combinator = python_file();
-    assert_parses_fast(&combinator, &file);
-}
+// use std::path::Path;
+// use std::time::Instant;
+// use jun3::python_file;
+// use jun3::utils::{assert_parses, assert_parses_fast};
+//
+// fn main() {
+//     println!("Running Python benchmarks...");
+//     let path = Path::new("src/tests/test_input.py");
+//     let file = std::fs::read_to_string(path).unwrap();
+//     let combinator = python_file();
+//     assert_parses_fast(&combinator, &file);
+// }
