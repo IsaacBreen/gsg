586398378b956f779040ee7e131c90c591d15262
IsaacBreen <mail@isaacbreen.com>
2024-08-24 16:19:47 +0800

    static

--- jul1/src/combinators/reference.rs
+++ jul1/src/combinators/reference.rs
@@ -70,7 +70,7 @@ impl<T> Hash for StrongRef<T> {
     }
 }
 
-impl<T: CombinatorTrait> DynCombinatorTrait for WeakRef<T> {
+impl<T: CombinatorTrait + 'static> DynCombinatorTrait for WeakRef<T> {
     fn parse_dyn(&self, right_data: RightData, bytes: &[u8]) -> (Box<dyn ParserTrait>, ParseResults) {
         todo!()
     }
@@ -80,7 +80,7 @@ impl<T: CombinatorTrait> DynCombinatorTrait for WeakRef<T> {
     }
 }
 
-impl<T: CombinatorTrait> CombinatorTrait for WeakRef<T> {
+impl<T: CombinatorTrait + 'static> CombinatorTrait for WeakRef<T> {
     type Parser<'a> = T::Parser<'a> where Self: 'a;
 
     fn one_shot_parse(&self, right_data: RightData, bytes: &[u8]) -> UnambiguousParseResults {
@@ -94,17 +94,16 @@ impl<T: CombinatorTrait> CombinatorTrait for WeakRef<T> {
     }
 }
 
-impl<T: CombinatorTrait> BaseCombinatorTrait for WeakRef<T> {
+impl<T: CombinatorTrait + 'static> BaseCombinatorTrait for WeakRef<T> {
     fn as_any(&self) -> &dyn std::any::Any {
-        // self
-        todo!()
+        self
     }
     fn apply_to_children(&self, f: &mut dyn FnMut(&dyn BaseCombinatorTrait)) {
         f(self.inner.upgrade().expect("WeakRef is already dropped").get().expect("Combinator hasn't been set"));
     }
 }
 
-impl<T: CombinatorTrait> DynCombinatorTrait for StrongRef<T> {
+impl<T: CombinatorTrait + 'static> DynCombinatorTrait for StrongRef<T> {
     fn parse_dyn(&self, right_data: RightData, bytes: &[u8]) -> (Box<dyn ParserTrait + '_>, ParseResults) {
         let (parser, parse_results) = self.parse(right_data, bytes);
         (Box::new(parser), parse_results)
@@ -115,7 +114,7 @@ impl<T: CombinatorTrait> DynCombinatorTrait for StrongRef<T> {
     }
 }
 
-impl<T: CombinatorTrait> CombinatorTrait for StrongRef<T> {
+impl<T: CombinatorTrait + 'static> CombinatorTrait for StrongRef<T> {
     type Parser<'a> = T::Parser<'a> where Self: 'a;
 
     fn one_shot_parse(&self, right_data: RightData, bytes: &[u8]) -> UnambiguousParseResults {
@@ -131,10 +130,9 @@ impl<T: CombinatorTrait> CombinatorTrait for StrongRef<T> {
     }
 }
 
-impl<T: CombinatorTrait> BaseCombinatorTrait for StrongRef<T> {
+impl<T: CombinatorTrait + 'static> BaseCombinatorTrait for StrongRef<T> {
     fn as_any(&self) -> &dyn std::any::Any {
-        // self
-        todo!()
+        self
     }
     fn apply_to_children(&self, f: &mut dyn FnMut(&dyn BaseCombinatorTrait)) {
         f(self.inner.get().unwrap());
