e5bb8d298099edc2d75f6501b0eeed031cf7de0d
IsaacBreen <mail@isaacbreen.com>
2024-08-28 11:17:59 +0800

    move eps and fail in together

--- jul1/src/combinators/eps.rs
+++ jul1/src/combinators/eps.rs
@@ -1,5 +1,5 @@
 use crate::parse_state::{ParseResultTrait, RightData};
-use crate::{BaseCombinatorTrait, DynCombinatorTrait, UnambiguousParseResults};
+use crate::{BaseCombinatorTrait, DynCombinatorTrait, UnambiguousParseError, UnambiguousParseResults};
 use crate::{CombinatorTrait, ParseResults, ParserTrait, U8Set};
 #[derive(Debug)]
 pub struct Eps;
@@ -49,4 +49,56 @@ impl ParserTrait for EpsParser {
 
 pub fn eps() -> Eps {
     Eps
+}
+
+
+#[repr(transparent)]
+#[derive(Debug)]
+pub struct FailParser;
+
+#[derive(Debug)]
+pub struct Fail;
+
+impl DynCombinatorTrait for Fail {
+    fn parse_dyn(&self, right_data: RightData, bytes: &[u8]) -> (Box<dyn ParserTrait + '_>, ParseResults) {
+        let (parser, parse_results) = self.parse(right_data, bytes);
+        (Box::new(parser), parse_results)
+    }
+
+    fn one_shot_parse_dyn<'a>(&'a self, right_data: RightData, bytes: &[u8]) -> UnambiguousParseResults {
+        self.one_shot_parse(right_data, bytes)
+    }
+}
+
+impl CombinatorTrait for Fail {
+    type Parser<'a> = FailParser;
+    type Output = ();
+
+    fn one_shot_parse(&self, right_data: RightData, bytes: &[u8]) -> UnambiguousParseResults {
+        Err(UnambiguousParseError::Fail)
+    }
+
+    fn old_parse(&self, right_data: RightData, bytes: &[u8]) -> (Self::Parser<'_>, ParseResults) {
+        (FailParser, ParseResults::empty_finished())
+    }
+}
+
+impl BaseCombinatorTrait for Fail {
+    fn as_any(&self) -> &dyn std::any::Any where Self: 'static {
+        self
+    }
+}
+
+impl ParserTrait for FailParser {
+    fn get_u8set(&self) -> U8Set {
+        panic!("FailParser.get_u8set() called")
+    }
+
+    fn parse(&mut self, bytes: &[u8]) -> ParseResults {
+        panic!("FailParser already consumed")
+    }
+}
+
+pub fn fail() -> Fail {
+    Fail
 }
