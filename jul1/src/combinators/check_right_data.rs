a3a099a9818214a1cf672240ddc3ee1de650d66d
IsaacBreen <mail@isaacbreen.com>
2024-08-28 11:19:03 +0800

    move mutate_right_data into check_right_data

--- jul1/src/combinators/check_right_data.rs
+++ jul1/src/combinators/check_right_data.rs
@@ -70,3 +70,69 @@ impl BaseCombinatorTrait for CheckRightData {
 pub fn check_right_data(run: impl Fn(&RightData) -> bool + 'static) -> CheckRightData {
     CheckRightData { run: Box::new(run) }
 }
+
+pub struct MutateRightData {
+    pub(crate) run: Box<dyn Fn(&mut RightData) -> bool>,
+}
+
+impl Hash for MutateRightData {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        let ptr = std::ptr::addr_of!(self.run) as *const ();
+        std::ptr::hash(ptr, state);
+    }
+}
+
+impl PartialEq for MutateRightData {
+    fn eq(&self, other: &Self) -> bool {
+        std::ptr::eq(&self.run, &other.run)
+    }
+}
+
+impl Eq for MutateRightData {}
+
+impl Debug for MutateRightData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("MutateRightData").finish()
+    }
+}
+
+impl DynCombinatorTrait for MutateRightData {
+    fn parse_dyn(&self, right_data: RightData, bytes: &[u8]) -> (Box<dyn ParserTrait + '_>, ParseResults) {
+        let (parser, parse_results) = self.parse(right_data, bytes);
+        (Box::new(parser), parse_results)
+    }
+
+    fn one_shot_parse_dyn<'a>(&'a self, right_data: RightData, bytes: &[u8]) -> UnambiguousParseResults {
+        self.one_shot_parse(right_data, bytes)
+    }
+}
+
+impl CombinatorTrait for MutateRightData {
+    type Parser<'a> = FailParser;
+    type Output = ();
+
+    fn one_shot_parse(&self, mut right_data: RightData, bytes: &[u8]) -> UnambiguousParseResults {
+        if (self.run)(&mut right_data) {
+            Ok(right_data)
+        } else {
+            Err(UnambiguousParseError::Fail)
+        }
+    }
+    fn old_parse(&self, mut right_data: RightData, bytes: &[u8]) -> (Self::Parser<'_>, ParseResults) {
+        if (self.run)(&mut right_data) {
+            (FailParser, ParseResults::new_single(right_data, true))
+        } else {
+            (FailParser, ParseResults::empty_finished())
+        }
+    }
+}
+
+impl BaseCombinatorTrait for MutateRightData {
+    fn as_any(&self) -> &dyn std::any::Any where Self: 'static {
+        self
+    }
+}
+
+pub fn mutate_right_data(run: impl Fn(&mut RightData) -> bool + 'static) -> MutateRightData {
+    MutateRightData { run: Box::new(run) }
+}
