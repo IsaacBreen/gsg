652b186922b02448200c6c4e8d51cf7bf273cd7d
IsaacBreen <mail@isaacbreen.com>
2024-09-27 16:59:11 +0800

    remove bitset256

--- sep1/src/lib.rs
+++ sep1/src/lib.rs
@@ -2,7 +2,6 @@ extern crate core;
 
 pub mod frozenset;
 pub mod charmap;
-pub mod bitset256;
 pub mod precompute;
 pub mod tokenizer_combinators;
 pub mod u8set;
--- sep1/src/u8set.rs
+++ sep1/src/u8set.rs
@@ -1,9 +1,9 @@
-use crate::bitset256::BitSet256;
 use std::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign};
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash, Ord, PartialOrd)]
 pub struct U8Set {
-    pub(crate) bitset: BitSet256,
+    pub(crate) x: u128,
+    pub(crate) y: u128,
 }
 
 impl Default for U8Set {
@@ -13,27 +13,93 @@ impl Default for U8Set {
 }
 
 impl U8Set {
-    pub(crate) fn from_u8(p0: u8) -> U8Set {
+    #[inline]
+    fn is_set(&self, index: u8) -> bool {
+        if index < 128 {
+            self.x & (1 << index) != 0
+        } else {
+            self.y & (1 << (index - 128)) != 0
+        }
+    }
+
+    #[inline]
+    fn set_bit(&mut self, index: u8) {
+        if index < 128 {
+            self.x |= 1 << index;
+        } else {
+            self.y |= 1 << (index - 128);
+        }
+    }
+
+    #[inline]
+    fn clear_bit(&mut self, index: u8) {
+        if index < 128 {
+            self.x &= !(1 << index);
+        } else {
+            self.y &= !(1 << (index - 128));
+        }
+    }
+
+    #[inline]
+    fn update(&mut self, other: &Self) {
+        self.x |= other.x;
+        self.y |= other.y;
+    }
+
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.x.count_ones() as usize + self.y.count_ones() as usize
+    }
+
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.x == 0 && self.y == 0
+    }
+
+    #[inline]
+    pub fn clear(&mut self) {
+        self.x = 0;
+        self.y = 0;
+    }
+
+    #[inline]
+    pub fn all() -> Self {
+        U8Set { x: u128::MAX, y: u128::MAX }
+    }
+
+    #[inline]
+    pub fn none() -> Self {
+        U8Set { x: 0, y: 0 }
+    }
+
+    #[inline]
+    pub fn new() -> Self {
+        Self::none()
+    }
+
+
+    pub fn from_u8(p0: u8) -> U8Set {
         let mut result = U8Set::none();
         result.insert(p0);
         result
     }
 
-    pub(crate) fn from_u8_range(start: u8, end: u8) -> U8Set {
-        U8Set::from_match_fn(move |i| start <= i && i <= end)
+
+    pub fn from_u8_range(start: u8, end: u8) -> U8Set {
+        Self::from_match_fn(move |i| start <= i && i <= end)
     }
 
-    pub(crate) fn from_char(p0: char) -> U8Set {
-        U8Set::from_chars(&p0.to_string())
+    pub fn from_char(p0: char) -> U8Set {
+        Self::from_chars(&p0.to_string())
     }
 
-    pub(crate) fn from_char_negation(p0: char) -> U8Set {
+    pub fn from_char_negation(p0: char) -> U8Set {
         let mut result = U8Set::none();
         result.insert(p0 as u8);
         result.complement()
     }
 
-    pub(crate) fn from_byte_range(range: impl IntoIterator<Item=u8>) -> U8Set {
+    pub fn from_byte_range(range: impl IntoIterator<Item = u8>) -> U8Set {
         let mut result = U8Set::none();
         for c in range {
             assert!(c <= 255, "Character {} is not a valid u8 value", c);
@@ -42,61 +108,37 @@ impl U8Set {
         result
     }
 
-    pub(crate) fn from_char_negation_range(range: impl IntoIterator<Item=u8>) -> U8Set {
-        U8Set::from_byte_range(range).complement()
+    pub fn from_char_negation_range(range: impl IntoIterator<Item = u8>) -> U8Set {
+        Self::from_byte_range(range).complement()
     }
-}
 
-impl U8Set {
+    #[inline]
     pub fn insert(&mut self, value: u8) -> bool {
         if self.contains(value) {
             false
         } else {
-            self.bitset.set_bit(value);
+            self.set_bit(value);
             true
         }
     }
 
+
+    #[inline]
     pub fn remove(&mut self, value: u8) -> bool {
         if !self.contains(value) {
             false
         } else {
-            self.bitset.clear_bit(value);
+            self.clear_bit(value);
             true
         }
     }
 
-    pub fn update(&mut self, other: &U8Set) {
-        self.bitset.update(&other.bitset);
-    }
 
+    #[inline]
     pub fn contains(&self, value: impl Into<u8>) -> bool {
-        self.bitset.is_set(value.into())
+        self.is_set(value.into())
     }
 
-    pub fn len(&self) -> usize {
-        self.bitset.len()
-    }
-
-    pub fn is_empty(&self) -> bool {
-        self.bitset.is_empty()
-    }
-
-    pub fn clear(&mut self) {
-        self.bitset.clear();
-    }
-
-    pub fn all() -> Self {
-        U8Set { bitset: BitSet256::all() }
-    }
-
-    pub fn new() -> Self {
-        U8Set { bitset: BitSet256::new() }
-    }
-
-    pub fn none() -> Self {
-        U8Set { bitset: BitSet256::none() }
-    }
 
     pub fn from_byte(byte: u8) -> Self {
         let mut result = Self::none();
@@ -121,22 +163,14 @@ impl U8Set {
         result
     }
 
+
     pub fn from_chars_negation(chars: &str) -> Self {
-        let mut result = Self::none();
-        for c in chars.chars() {
-            assert!(c as usize <= 255, "Character {} is not a valid u8 value", c);
-            result.insert(c as u8);
-        }
-        result.complement()
+        Self::from_chars(chars).complement()
     }
 
+
     pub fn from_str(s: &str) -> Self {
-        let mut result = Self::none();
-        for c in s.chars() {
-            assert!(c as usize <= 255, "Character {} is not a valid u8 value", c);
-            result.insert(c as u8);
-        }
-        result
+        Self::from_chars(s)
     }
 
     pub fn from_match_fn<F>(f: F) -> Self
@@ -153,31 +187,45 @@ impl U8Set {
     }
 
     pub fn from_range(start: u8, end: u8) -> Self {
-        U8Set::from_match_fn(move |i| start <= i && i <= end)
+        Self::from_match_fn(move |i| start <= i && i <= end)
     }
 
-    pub fn iter(&self) -> impl Iterator<Item=u8> + '_ {
+    pub fn iter(&self) -> impl Iterator<Item = u8> + '_ {
         (0..=255).filter(move |&i| self.contains(i))
     }
 
+    #[inline]
     pub fn union(&self, other: &Self) -> Self {
-        U8Set { bitset: self.bitset.union(&other.bitset) }
+        U8Set {
+            x: self.x | other.x,
+            y: self.y | other.y,
+        }
     }
 
+    #[inline]
     pub fn intersection(&self, other: &Self) -> Self {
-        U8Set { bitset: self.bitset.intersection(&other.bitset) }
+        U8Set {
+            x: self.x & other.x,
+            y: self.y & other.y,
+        }
     }
 
+
+    #[inline]
     pub fn complement(&self) -> Self {
-        U8Set { bitset: self.bitset.complement() }
+        U8Set {
+            x: !self.x,
+            y: !self.y,
+        }
     }
 }
+
 
 impl BitOr for &U8Set {
     type Output = U8Set;
 
     fn bitor(self, other: &U8Set) -> U8Set {
-        U8Set { bitset: self.bitset.clone() | other.bitset.clone() }
+        self.union(other)
     }
 }
 
@@ -185,9 +233,7 @@ impl BitAnd for &U8Set {
     type Output = U8Set;
 
     fn bitand(self, other: &U8Set) -> U8Set {
-        U8Set {
-            bitset: self.bitset.clone() & other.bitset.clone(),
-        }
+        self.intersection(other)
     }
 }
 
@@ -215,7 +261,8 @@ impl BitOrAssign for U8Set {
 
 impl BitAndAssign for U8Set {
     fn bitand_assign(&mut self, other: U8Set) {
-        self.bitset &= other.bitset;
+        self.x &= other.x;
+        self.y &= other.y;
     }
 }
 
