d76a4647455e9bbbc5a897a977c7432e853e47f5
IsaacBreen <mail@isaacbreen.com>
2024-11-02 09:12:30 +0800

    nice

--- nov1/src/constrained_generation.rs
+++ nov1/src/constrained_generation.rs
@@ -1,10 +1,17 @@
 use crate::glr::parser::GLRParserState;
 use crate::precompute::{StateID, Token};
 use std::collections::BTreeMap;
+use crate::glr::table::TerminalID;
+
+type LLMToken = usize;
 
 struct GrammarConstraintState<'a> {
     precomputed: BTreeMap<StateID, BTreeMap<Vec<Token>, BTreeMap<&'a [u8], usize>>>,
-    parser_state: GLRParserState<'a>,
+    // Somehow we need to associate the tokenizer state with each parser state.
+    // We could maybe add a generic to the parser state.
+    // Or maybe we could 'thin out' GLRParserState and its related methods so we can deal with ParseState here directly.
+    // Perhaps option 2 is better.
+    parser_states: GLRParserState<'a>,
 }
 
 impl GrammarConstraintState<'_> {
@@ -13,6 +20,10 @@ impl GrammarConstraintState<'_> {
     }
 
     fn get_mask(&self) -> Vec<bool> {
+        todo!()
+    }
+
+    fn step(&mut self, llm_token: LLMToken) {
         todo!()
     }
 }
